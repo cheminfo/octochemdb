cba12b9aad5cdf12c72e518cc8d736d806d80f7c
Luc Patiny <luc@patiny.com>
2020-05-09 15:42:35 +0200

    feat: add moleculesFromSmiles

---
+++ node-pubchem/src/src/search/moleculesFromSmiles.js
@@ -0,0 +1,54 @@
+'use strict';
+
+// query for molecules from monoisotopic mass
+const pubChemConnection = new (require('../util/PubChemConnection'))();
+
+/**
+ * Find molecules from a monoisotopic mass
+ * @param {number} smiles
+ * @param {object} [options={}]
+ * @param {object} [options.noStereo=true]
+ * @return {Array}
+ */
+module.exports = async function moleculesFromSmiles(smiles, options = {}) {
+  let { limit = 1e3, noStereo = true } = options;
+
+  if (!smiles) {
+    throw new Error('smiles parameter must be specified');
+  }
+
+  const molecule = Molecule.fromSmiles(smiles);
+  let mongoQuery = {};
+  if (noStereo) {
+    molecule.stripStereoInformation();
+    idCode = molecule.getIDCode();
+    mongoQuery = {
+      noStereoID: idCode,
+    };
+  } else {
+    idCode = molecule.getIDCode();
+    mongoQuery = {
+      ocl: { id: idCode },
+    };
+  }
+
+  const collection = await pubChemConnection.getMoleculesCollection();
+
+  return collection
+    .aggregate([
+      { $match: mongoQuery },
+      { $limit: Number(limit) },
+      {
+        $project: {
+          id: '$_id',
+          iupac: 1,
+          ocl: 1,
+          mf: 1,
+          em: 1,
+          nbFragments: 1,
+          charge: 1,
+        },
+      },
+    ])
+    .toArray();
+};
