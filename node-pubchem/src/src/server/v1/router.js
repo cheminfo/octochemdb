ff1ed807ce53c75a58bba31c738c65edb8eb53dd
Luc Patiny <luc@patiny.com>
2021-10-15 17:52:49 +0200

    chore: working on fastify migration

---
+++ node-pubchem/src/src/server/v1/router.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const router = require('koa-router')();
+
+router.get('/mfs/em', async (ctx) => {
+  const search = require('../../search/mfsFromEm');
+  const result = await search(ctx.request.query.em, ctx.request.query);
+  ctx.body = {
+    result,
+  };
+});
+
+router.get('/molecules/em', async (ctx) => {
+  const search = require('../../search/moleculesFromEm');
+  const result = await search(ctx.request.query.em, ctx.request.query);
+  ctx.body = {
+    result,
+  };
+});
+
+router.get('/molecules/mf', async (ctx) => {
+  const search = require('../../search/moleculesFromMf');
+  const result = await search(ctx.request.query.mf, ctx.request.query);
+  ctx.body = {
+    result,
+  };
+});
+
+router.get('/molecules/smiles', async (ctx) => {
+  const search = require('../../search/moleculesFromSmiles');
+  const result = await search(ctx.request.query.smiles, ctx.request.query);
+  ctx.body = {
+    result,
+  };
+});
+
+/*
+router.get('/mfStats/search', async () => {
+  this.body = {
+    result: await api.mfStats.search(this.query)
+  };
+});
+*/
+
+/*
+router.get('/mfStats/toc', () => {
+  this.body = {
+    result: await api.mfStats.toc(this.query)
+  };
+});
+*/
+
+module.exports = router;
