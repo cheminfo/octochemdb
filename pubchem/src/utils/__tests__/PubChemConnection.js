56543c5d52b4a8ea52e9ee0c74d0d244212e8828
Luc Patiny <luc@patiny.com>
2022-02-22 14:28:26 +0100

    test: rename PubChemConnection

--- pubchem/src/utils/__tests__/PubChemConnection.js
+++
@@ -1,8 +0,0 @@
-
-
-const pubChemConnection = new (require('../PubChemConnection'))();
-
-test('connection to DB', async () => {
-  let database = await pubChemConnection.getDatabase();
-  expect(database.databaseName).toBe('pubchem');
-});
