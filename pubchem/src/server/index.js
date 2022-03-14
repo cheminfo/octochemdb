4654f9f4df772152ba736fd1629d054a29b1d751
Luc Patiny <luc@patiny.com>
2022-03-14 09:36:08 +0100

    feat: add stats

--- pubchem/src/server/index.js
+++ pubchem/src/server/index.js
@@ -1,10 +1,15 @@
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+import fastifyStatic from 'fastify-static';
 import fastifySwagger from 'fastify-swagger';
 
 import { recursiveDir } from '../utils/recursiveDir.js';
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export default async function setupV1(app, _, done) {
   app.register(fastifySwagger, {
-    routePrefix: '/documentation',
     swagger: {
       info: {
         title: 'Search a copy of pubchem database',
@@ -15,8 +20,9 @@ export default async function setupV1(app, _, done) {
     exposeRoute: true,
   });
 
-  app.get('/', { schema: { hide: true } }, (_, reply) => {
-    reply.redirect('/v1/documentation');
+  app.register(fastifyStatic, {
+    root: join(__dirname, 'public'),
+    prefix: '/public/', // optional: default '/'
   });
 
   const url = new URL('../plugins/', import.meta.url);
