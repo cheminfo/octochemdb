ad206d2a108e5d88ce4d25bc829fd159406ed31b
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-03-30 10:55:33 +0200

    fix: process.env.TELEGRAM_CHAT_ID should return error only if is false

--- pubchem/src/utils/sendTelegram.js
+++ pubchem/src/utils/sendTelegram.js
@@ -4,7 +4,7 @@ import Debug from 'debug';
 const debug = Debug('sendTelegram');
 
 export async function sendTelegram(message) {
-  if (!process.env.TELEGRAM_BOT_ID || process.env.TELEGRAM_CHAT_ID) {
+  if (!process.env.TELEGRAM_BOT_ID || !process.env.TELEGRAM_CHAT_ID) {
     debug(
       'no TELEGRAM_CHAT_ID or TELEGRAM_BOT_ID environnement variable Can not send telegram',
     );
