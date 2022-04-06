513174f05afd7fca283758af2b0a835d2e91f8a6
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-04-06 15:15:59 +0200

    chore: introduced new cmaup function to perform reading prelimirary operations like reading files and get collection

--- pubchem/src/plugins/cmaup/sync/syncCmaup.js
+++ pubchem/src/plugins/cmaup/sync/syncCmaup.js
@@ -1,71 +1,25 @@
 import { readFileSync } from 'fs';
 
-import pkg from 'papaparse';
-
 import Debug from '../../../utils/Debug.js';
 
 import { parseCmaup } from './utils/parseCmaup.js';
-import getLastFileSync from '../../../sync/http/utils/getLastFileSync.js';
-const { parse } = pkg;
+import cmaupStartSyning from './utils/cmaupStartSyning.js';
 
 const debug = Debug('syncCmaup');
 
 export async function sync(connection) {
-  let options = {
-    collectionSource: process.env.CMAUP_SOURCE_INGREDIENTS,
-    destinationLocal: `${process.env.ORIGINAL_DATA_PATH}/cmaup/full`,
-    collectionName: 'cmaup',
-    filenameNew: 'Ingredients',
-    extensionNew: 'txt',
-  };
-  const lastFile = await getLastFileSync(options);
-  options.collectionSource = process.env.CMAUP_SOURCE_ACTIVITY;
-  options.filenameNew = 'activity';
-  const lastFileActivity = await getLastFileSync(options);
-  options.collectionSource = process.env.CMAUP_SOURCE_SPECIESASSOCIATION;
-  options.filenameNew = 'speciesAssociation';
-  const lastFileSpeciesAssociation = await getLastFileSync(options);
-  options.collectionSource = process.env.CMAUP_SOURCE_SPECIESINFO;
-  options.filenameNew = 'speciesInfo';
-  const lastFileSpeciesInfo = await getLastFileSync(options);
-  const progress = await connection.getProgress('cmaup');
-  const collection = await connection.getCollection('cmaup');
-  await collection.createIndex({ 'data.ocl.id': 1 });
-  await collection.createIndex({ 'data.ocl.noStereoID': 1 });
-  const lastDocumentImported = await getLastCMAUPImported(connection, progress);
-
-  let firstID;
-  if (
-    lastDocumentImported &&
-    lastDocumentImported._source &&
-    lastFile.includes(lastDocumentImported._source)
-  ) {
-    firstID = lastDocumentImported._id;
-  }
-  const general = parse(readFileSync(lastFile, 'utf8'), {
-    header: true,
-  }).data;
-  const activities = {};
-  parse(readFileSync(lastFileActivity, 'utf8'), {
-    header: true,
-  }).data.forEach((entry) => {
-    if (!activities[entry.Ingredient_ID]) {
-      activities[entry.Ingredient_ID] = [];
-    }
-    activities[entry.Ingredient_ID].push(entry);
-  });
-
-  const speciesPair = parse(readFileSync(lastFileSpeciesAssociation, 'utf8'), {
-    header: false,
-  }).data;
-
-  const speciesInfo = {};
-  parse(readFileSync(lastFileSpeciesInfo, 'utf8'), {
-    header: true,
-  }).data.forEach((entry) => (speciesInfo[entry.Plant_ID] = entry));
-
-  // we reparse all the file and skip if required
-  const source = lastFile.replace(process.env.ORIGINAL_DATA_PATH, '');
+  const {
+    firstID,
+    lastDocumentImported,
+    progress,
+    source,
+    collection,
+    general,
+    activities,
+    speciesPair,
+    speciesInfo,
+    lastFile,
+  } = await cmaupStartSyning(connection);
   let skipping = firstID !== undefined;
   let counter = 0;
   let imported = 0;
@@ -117,13 +71,4 @@ export async function sync(connection) {
     _source: { $ne: source },
   });
   debug(`Deleting entries with wrong source: ${result.deletedCount}`);
-}
-
-async function getLastCMAUPImported(connection, progress) {
-  const collection = await connection.getCollection('cmaup');
-  return collection
-    .find({ _seq: { $lte: progress.seq } })
-    .sort('_seq', -1)
-    .limit(1)
-    .next();
 }
---
+++ pubchem/src/plugins/cmaup/sync/utils/cmaupStartSyning.js
@@ -0,0 +1,79 @@
+import getLastFileSync from '../../../../sync/http/utils/getLastFileSync.js';
+import getLastDocumentImported from '../../../../sync/http/utils/getLastDocumentImported.js';
+import pkg from 'papaparse';
+import { readFileSync } from 'fs';
+
+const { parse } = pkg;
+
+async function cmaupStartSyning(connection) {
+  let options = {
+    collectionSource: process.env.CMAUP_SOURCE_INGREDIENTS,
+    destinationLocal: `${process.env.ORIGINAL_DATA_PATH}/cmaup/full`,
+    collectionName: 'cmaup',
+    filenameNew: 'Ingredients',
+    extensionNew: 'txt',
+  };
+  const lastFile = await getLastFileSync(options);
+  options.collectionSource = process.env.CMAUP_SOURCE_ACTIVITY;
+  options.filenameNew = 'activity';
+  const lastFileActivity = await getLastFileSync(options);
+  options.collectionSource = process.env.CMAUP_SOURCE_SPECIESASSOCIATION;
+  options.filenameNew = 'speciesAssociation';
+  const lastFileSpeciesAssociation = await getLastFileSync(options);
+  options.collectionSource = process.env.CMAUP_SOURCE_SPECIESINFO;
+  options.filenameNew = 'speciesInfo';
+  const lastFileSpeciesInfo = await getLastFileSync(options);
+  const source = lastFile.replace(process.env.ORIGINAL_DATA_PATH, '');
+  const progress = await connection.getProgress('cmaup');
+  const collection = await connection.getCollection('cmaup');
+  await collection.createIndex({ 'data.ocl.id': 1 });
+  await collection.createIndex({ 'data.ocl.noStereoID': 1 });
+  const lastDocumentImported = await getLastDocumentImported(
+    connection,
+    progress,
+    options.collectionName,
+  );
+
+  let firstID;
+  if (
+    lastDocumentImported &&
+    lastDocumentImported._source &&
+    lastFile.includes(lastDocumentImported._source)
+  ) {
+    firstID = lastDocumentImported._id;
+  }
+  const general = parse(readFileSync(lastFile, 'utf8'), {
+    header: true,
+  }).data;
+  const activities = {};
+  parse(readFileSync(lastFileActivity, 'utf8'), {
+    header: true,
+  }).data.forEach((entry) => {
+    if (!activities[entry.Ingredient_ID]) {
+      activities[entry.Ingredient_ID] = [];
+    }
+    activities[entry.Ingredient_ID].push(entry);
+  });
+
+  const speciesPair = parse(readFileSync(lastFileSpeciesAssociation, 'utf8'), {
+    header: false,
+  }).data;
+
+  const speciesInfo = {};
+  parse(readFileSync(lastFileSpeciesInfo, 'utf8'), {
+    header: true,
+  }).data.forEach((entry) => (speciesInfo[entry.Plant_ID] = entry));
+  return {
+    firstID,
+    lastDocumentImported,
+    progress,
+    source,
+    collection,
+    general,
+    activities,
+    speciesPair,
+    speciesInfo,
+    lastFile,
+  };
+}
+export default cmaupStartSyning;
