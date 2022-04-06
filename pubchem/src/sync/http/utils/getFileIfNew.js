96465124350ff3a831700f4b44e3f2218b542598
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-04-06 12:56:51 +0200

    feat: check if local file size is lower than file to be download before proceding to download

--- pubchem/src/sync/http/utils/getFileIfNew.js
+++ pubchem/src/sync/http/utils/getFileIfNew.js
@@ -1,6 +1,6 @@
 import { utimesSync, existsSync, createWriteStream, rmSync } from 'fs';
 import { join } from 'path';
-
+import { fileListFromPath } from 'filelist-from';
 import fetch from 'cross-fetch';
 import FSExtra from 'fs-extra';
 
@@ -29,37 +29,59 @@ async function getFileIfNew(file, targetFolder, options = {}) {
     const response = await fetch(file.url, { signal: controller.signal });
 
     const headers = Array.from(response.headers);
-
     let lastMofidied =
       headers.filter((row) => row[0] === 'last-modified')[0] ||
       headers.filter((row) => row[0] === 'date')[0];
 
-    let modificationDate = new Date(lastMofidied[1])
-      .toISOString()
-      .substring(0, 10);
-    debug(`Last modification date: ${modificationDate}`);
-
-    const targetFile = join(
-      targetFolder,
-      `${filename}.${modificationDate}.${extension}`,
+    let newFileSize = Number(
+      headers.filter((row) => row[0] === 'content-length')[0][1],
     );
-    target = targetFile;
-    debug(`targetFile: ${targetFile}`);
-    if (existsSync(targetFile)) {
-      debug('file already exists, no need to fetch');
+    let fileList = fileListFromPath(targetFolder).filter((file) =>
+      file.name.includes(('.zip' || '.txt' || '.json') && filename),
+    );
+    let lastFilesSize = fileList.sort((a, b) => a.size - b.size)[0].size;
+
+    let lastFileTargetLocal = fileList.filter(
+      (file) => file.size === lastFilesSize,
+    )[0].name;
+
+    if (lastFilesSize !== newFileSize) {
+      let modificationDate = new Date(lastMofidied[1])
+        .toISOString()
+        .substring(0, 10);
+      debug(`Last modification date: ${modificationDate}`);
+
+      const targetFile = join(
+        targetFolder,
+        `${filename}.${modificationDate}.${extension}`,
+      );
+      debug(`targetFile: ${targetFile}`);
+
+      debug(
+        'New file size do not match local one:' +
+          newFileSize +
+          '/' +
+          lastFilesSize,
+      );
+      const body = response.body;
+      const encoding = body._readableState.defaultEncoding;
+      const writeStream = createWriteStream(targetFile, encoding);
+      for await (let part of body) {
+        writeStream.write(part);
+      }
+      writeStream.close();
+      if (file.epoch) utimesSync(targetFile, file.epoch, file.epoch);
+
+      debug(`Downloading: ${options.filename}`);
+      target = targetFile;
+      return targetFile;
+    } else {
+      const targetFile = join(targetFolder, lastFileTargetLocal);
+      debug(
+        'New file size match local one:' + newFileSize + '/' + lastFilesSize,
+      );
       return targetFile;
     }
-    const body = response.body;
-    const encoding = body._readableState.defaultEncoding;
-    const writeStream = createWriteStream(targetFile, encoding);
-    for await (let part of body) {
-      writeStream.write(part);
-    }
-    writeStream.close();
-    if (file.epoch) utimesSync(targetFile, file.epoch, file.epoch);
-
-    debug(`Downloading: ${options.filename}`);
-    return targetFile;
   } catch (e) {
     debug(`ERROR downloading: ${options.filename}`);
     debug(target);
