1c0bb01ae89dc26edff6940840e3e40d46d5620a
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-04-10 17:46:44 +0200

    wip: decompress huge gzip files using stream #36

---
+++ pubchem/src/plugins/relationPubs/sync/utils/gunzipStream.js
@@ -0,0 +1,20 @@
+import { createGunzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
+import { join } from 'path';
+const pipe = promisify(pipeline);
+
+async function gunzipStream(input, output) {
+  const gzip = createGunzip();
+  const source = createReadStream(input);
+  const destination = createWriteStream(join(output, '.txt'));
+  await pipe(source, gzip, destination).catch((err) => {
+    console.error('An error occurred:', err);
+    process.exitCode = 1;
+  });
+
+  return output;
+}
+
+export default gunzipStream;
