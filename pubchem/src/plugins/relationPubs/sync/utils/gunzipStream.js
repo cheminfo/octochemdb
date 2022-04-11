9aa5505ae5f3612470c09d5574aafc38af2bedb7
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-04-11 12:10:45 +0200

    fix: avoid to decompress file if it was already done

--- pubchem/src/plugins/relationPubs/sync/utils/gunzipStream.js
+++ pubchem/src/plugins/relationPubs/sync/utils/gunzipStream.js
@@ -1,18 +1,25 @@
 import { createGunzip } from 'zlib';
-import { createReadStream, createWriteStream } from 'fs';
+import { createReadStream, createWriteStream, existsSync } from 'fs';
 import { pipeline } from 'stream';
 import { promisify } from 'util';
 import { join } from 'path';
-const pipe = promisify(pipeline);
+import Debug from '../../../../utils/Debug.js';
 
+const pipe = promisify(pipeline);
+const debug = Debug('gunzipStream');
 async function gunzipStream(input, output) {
-  const gzip = createGunzip();
-  const source = createReadStream(input);
-  const destination = createWriteStream(join(`${output}.txt`));
-  await pipe(source, gzip, destination).catch((err) => {
-    console.error('An error occurred:', err);
-    process.exitCode = 1;
-  });
+  if (!existsSync(input)) {
+    debug(`decompress ${input}`);
+    const gzip = createGunzip();
+    const source = createReadStream(input);
+    const destination = createWriteStream(join(`${output}.txt`));
+    await pipe(source, gzip, destination).catch((err) => {
+      console.error('An error occurred:', err);
+      process.exitCode = 1;
+    });
+  } else {
+    debug('file already decompressed');
+  }
 
   return join(`${output}.txt`);
 }
