2dee39903bf5c7244031597e6d8353609ab1a1bd
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-04-14 09:03:55 +0200

    feat: introduced logs collections

--- pubchem/src/plugins/cmaup/sync/syncCmaup.js
+++ pubchem/src/plugins/cmaup/sync/syncCmaup.js
@@ -10,35 +10,24 @@ export async function sync(connection) {
     firstID,
     lastDocumentImported,
     progress,
-    source,
+    sources,
     collection,
     general,
     activities,
     speciesPair,
     speciesInfo,
-    newFiles,
+    logs,
   } = await cmaupStartSync(connection);
+
   let skipping = firstID !== undefined;
   let counter = 0;
   let imported = 0;
   let start = Date.now();
-  let oldSource;
-  if (lastDocumentImported !== null) {
-    oldSource = lastDocumentImported._source;
-  } else {
-    oldSource = [' '];
-  }
-
-  let status = false;
-  for (let i = 0; i < newFiles.length; i++) {
-    if (newFiles[i].includes(oldSource[i])) status = true;
-    if (!status) break;
-  }
 
   // Reimport collection again only if lastDocument imported changed or importation was not completed
   if (
     lastDocumentImported === null ||
-    !status ||
+    sources !== logs.sourcesHash ||
     progress.state !== 'updated'
   ) {
     debug(`Start parsing cmaup`);
@@ -51,11 +40,6 @@ export async function sync(connection) {
       counter++;
       // If test mode break process after counter >20
       if (process.env.TEST === 'true' && counter > 20) break;
-      // Log dubug each 10s or defined time
-      if (Date.now() - start > Number(process.env.DEBUG_THROTTLING || 10000)) {
-        debug(`Processing: counter: ${counter} - imported: ${imported}`);
-        start = Date.now();
-      }
       // Skipp importation till last id imported if collection was not fully updated
       if (skipping && progress.state !== 'updated') {
         if (firstID === entry._id) {
@@ -64,18 +48,27 @@ export async function sync(connection) {
         }
         continue;
       }
+      // Log dubug each 10s or defined time
+      if (Date.now() - start > Number(process.env.DEBUG_THROTTLING || 10000)) {
+        debug(`Processing: counter: ${counter} - imported: ${imported}`);
+        start = Date.now();
+      }
 
       entry._seq = ++progress.seq;
-      entry._source = source;
+      progress.state = 'updating';
       await collection.updateOne(
         { _id: entry._id },
         { $set: entry },
         { upsert: true },
       );
-      progress.state = 'updating';
       await connection.setProgress(progress);
       imported++;
     }
+    logs.dateEnd = Date.now();
+    logs.endSequenceID = progress.seq;
+    logs.status = 'updated';
+    await connection.updateImportationLog(logs);
+    progress.sources = sources;
     progress.date = new Date();
     progress.state = 'updated';
     await connection.setProgress(progress);
@@ -85,7 +78,7 @@ export async function sync(connection) {
   }
   // we remove all the entries that are not imported by the last file
   const result = await collection.deleteMany({
-    _source: { $ne: source },
+    _seq: { $lte: logs.startSequenceID },
   });
   debug(`Deleting entries with wrong source: ${result.deletedCount}`);
 }
