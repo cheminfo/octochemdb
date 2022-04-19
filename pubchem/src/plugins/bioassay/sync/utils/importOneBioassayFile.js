5e606a7c2db9a5ddd5fec05acc5af36b05d7f039
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-04-19 12:02:24 +0200

    wip: implemented aggregation while importing because for one SID there could be multiple bioassays results #45

--- pubchem/src/plugins/bioassay/sync/utils/importOneBioassayFile.js
+++ pubchem/src/plugins/bioassay/sync/utils/importOneBioassayFile.js
@@ -3,7 +3,7 @@ import { readFileSync } from 'fs';
 import { fileListFromZip, fileListUngzip } from 'filelist-utils';
 import parseOneFile from './parseOneFile.js';
 import Debug from '../../../../utils/Debug.js';
-import path from 'path';
+import md5 from 'md5';
 const debug = Debug('importOneBioassayFile');
 
 export default async function importOneBioassayFile(
@@ -18,14 +18,49 @@ export default async function importOneBioassayFile(
   const data = readFileSync(file.path);
   const fileList = await fileListFromZip(data);
   const ungzippedFileList = await fileListUngzip(fileList);
-  for await (let entry of parseOneFile(ungzippedFileList)) {
-    entry._id = ++progress.seq;
-    progress.state = 'updating';
-    await collection.updateOne(
-      { _id: entry._id },
-      { $set: entry },
-      { upsert: true },
-    );
-    await connection.setProgress(progress);
+  let countAggregate = 0;
+  for (let file of ungzippedFileList) {
+    for await (let entry of parseOneFile(file)) {
+      // entry._id = ++progress.seq;
+      progress.state = 'updating';
+
+      let aggregateBySid = await collection
+        .aggregate([
+          {
+            $match: {
+              _id: entry._id,
+            },
+          },
+        ])
+        .toArray();
+      if (aggregateBySid.length > 0) {
+        if (
+          md5(entry._data.references) !==
+          md5(aggregateBySid[0]._data.references)
+        ) {
+          entry._data.references.push(aggregateBySid[0]._data.references);
+        }
+        if (
+          md5(entry._data.bioassays) == md5(aggregateBySid[0]._data.bioassays)
+        ) {
+          entry._data.bioassays.push(aggregateBySid[0]._data.bioassays);
+        }
+
+        countAggregate++;
+        debug(`aggregated:${countAggregate}`);
+        await collection.updateOne(
+          { _id: entry._id },
+          { $set: entry },
+          { upsert: true },
+        );
+      } else {
+        await collection.updateOne(
+          { _id: entry._id },
+          { $set: entry },
+          { upsert: true },
+        );
+      }
+      await connection.setProgress(progress);
+    }
   }
 }
--- pubchem/src/plugins/bioassay/sync/utils/parseOneFile.js
+++ pubchem/src/plugins/bioassay/sync/utils/parseOneFile.js
@@ -1,73 +1,90 @@
-async function* parseOneFile(files) {
-  for await (let file of files) {
-    try {
-      const data = await file.text();
-      const object = JSON.parse(data);
-      let testedSubstances = object.PC_AssaySubmit.data;
-      for (const entry of testedSubstances) {
-        let test_id = object.PC_AssaySubmit.assay.descr?.results;
-        let testResults = [];
-        for (const test of test_id) {
-          let testResult = {};
-          for (const outcome of entry.data) {
-            if (test.tid === outcome.tid) {
-              testResult.name = test.name;
-              testResult.description = test.description;
-              testResult.result = outcome.value;
-            }
-          }
-          if (testResult.name) {
-            testResults.push(testResult);
-          }
-        }
-        let result = {};
-        result.sid = entry.sid;
-        if (object.PC_AssaySubmit.assay.descr?.name) {
-          result.assayDescription = {
-            name: object.PC_AssaySubmit.assay.descr?.name,
-          };
-          if (
-            object.PC_AssaySubmit.assay.descr?.protocol &&
-            JSON.stringify(object.PC_AssaySubmit.assay.descr?.protocol) !==
-              JSON.stringify([''])
-          ) {
-            result.assayDescription.protocol =
-              object.PC_AssaySubmit.assay.descr?.protocol;
-          }
-          if (object.PC_AssaySubmit.assay.descr?.comment) {
-            result.assayDescription.comment =
-              object.PC_AssaySubmit.assay.descr?.comment;
-          }
-        }
-        if (testResults.length > 0) {
-          result.assayResults = testResults;
-        }
+import Debug from '../../../../utils/Debug.js';
+const debug = Debug('parseOneFile');
+async function* parseOneFile(file) {
+  try {
+    const data = await file.text();
+    const object = JSON.parse(data);
+    let testedSubstances = object.PC_AssaySubmit.data;
+    for await (const entry of testedSubstances) {
+      let test_id = object.PC_AssaySubmit.assay.descr?.results;
+      let testResults = [];
+      for (const test of test_id) {
+        let testResult = {};
 
-        if (entry?.outcome) {
-          if (entry?.outcome === 1) {
-            result.activityOutcome = 'chemical probe';
-          }
-          if (entry?.outcome === 2) {
-            result.activityOutcome = 'active';
-          }
-          if (entry?.outcome === 3) {
-            result.activityOutcome = 'inactive';
-          }
-          if (entry?.outcome === 4) {
-            result.activityOutcome = 'inconclusive';
-          }
-          if (entry?.outcome === 5) {
-            result.activityOutcome = 'unspecified';
+        for (const outcome of entry.data) {
+          if (test.tid === outcome.tid) {
+            testResult.name = test.name;
+            testResult.description = test.description;
+            testResult.result = outcome.value;
           }
         }
-        if (object.PC_AssaySubmit.assay.descr?.xref) {
-          result._assayReferences = object.PC_AssaySubmit.assay.descr?.xref;
+        if (testResult.name) {
+          testResults.push(testResult);
         }
-        yield result;
       }
-    } catch (e) {
-      continue;
+      let result = {
+        _id: entry.sid,
+        _data: {
+          bioassays: [],
+          references: [],
+        },
+      };
+      let description = {};
+
+      if (object.PC_AssaySubmit.assay.descr?.name) {
+        description.name = object.PC_AssaySubmit.assay.descr?.name;
+      }
+      if (
+        object.PC_AssaySubmit.assay.descr?.protocol &&
+        JSON.stringify(object.PC_AssaySubmit.assay.descr?.protocol) !==
+          JSON.stringify([''])
+      ) {
+        description.protocol = object.PC_AssaySubmit.assay.descr?.protocol;
+      }
+      if (object.PC_AssaySubmit.assay.descr?.comment) {
+        description.comment = object.PC_AssaySubmit.assay.descr?.comment;
+      }
+      if (testResults.length > 0) {
+        description.results = testResults;
+      }
+      if (entry?.outcome) {
+        if (entry?.outcome === 1) {
+          description.activityOutcome = ['chemical probe'];
+        }
+        if (entry?.outcome === 2) {
+          description.activityOutcome = ['active'];
+        }
+        if (entry?.outcome === 3) {
+          description.activityOutcome = ['inactive'];
+        }
+        if (entry?.outcome === 4) {
+          description.activityOutcome = ['inconclusive'];
+        }
+        if (entry?.outcome === 5) {
+          description.activityOutcome = ['unspecified'];
+        }
+      }
+      result._data.bioassays.push(description);
+
+      if (object.PC_AssaySubmit.assay.descr?.xref) {
+        let reference = {
+          pmids: [],
+        };
+        for (const ref of object.PC_AssaySubmit.assay.descr?.xref) {
+          let property = Object.keys(ref.xref)[0];
+          if (property === 'pmid') {
+            reference.pmids.push(ref.xref[property]);
+          } else {
+            reference[property] = ref.xref[property];
+          }
+        }
+        result._data.references.push(reference);
+      }
+
+      yield result;
     }
+  } catch (e) {
+    debug(e);
   }
 }
 
