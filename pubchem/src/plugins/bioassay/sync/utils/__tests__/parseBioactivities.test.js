69b9cc3532bd11d7f8f7e2cc765b087d8c69f248
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-04-20 16:42:41 +0200

    chore: test case for parse function
    close: #45

---
+++ pubchem/src/plugins/bioassay/sync/utils/__tests__/parseBioactivities.test.js
@@ -0,0 +1,20 @@
+import parseBioactivities from '../parseBioactivities.js';
+import { join } from 'path';
+// remove process.env.TEST from parseBioactivities to use this test
+test('parseBioactivities', async () => {
+  const activity = join(
+    __dirname,
+    '../../../../../../../originalData/bioassay/full/bioactivities.2022-04-17.gz',
+  );
+  const bioassays = join(
+    __dirname,
+    '../../../../../../../originalData/bioassay/full/bioassays.2022-04-17.gz',
+  );
+  console.log(bioassays);
+  let results = [];
+  for await (const result of parseBioactivities(activity, bioassays)) {
+    if (results.length > 10) break;
+    results.push(result);
+  }
+  expect(results).toMatchSnapshot();
+});
