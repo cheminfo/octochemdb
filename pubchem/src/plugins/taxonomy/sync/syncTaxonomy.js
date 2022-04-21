9ee27f34fbbbd6aed2265992bf523866bf38f3f1
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-04-21 11:44:08 +0200

    chore: updated syncTaxonomy with function used in others sync functions

--- pubchem/src/plugins/taxonomy/sync/syncTaxonomy.js
+++ pubchem/src/plugins/taxonomy/sync/syncTaxonomy.js
@@ -1,50 +1,55 @@
 import { readFileSync } from 'fs';
-
+import md5 from 'md5';
 import { fileListFromZip } from 'filelist-from';
-
-import getFileIfNew from '../../../sync/http/utils/getFileIfNew.js';
+import getLastDocumentImported from '../../../sync/http/utils/getLastDocumentImported.js';
 import Debug from '../../../utils/Debug.js';
-
+import getLastFileSync from '../../../sync/http/utils/getLastFileSync.js';
 import { taxonomyParser } from './utils/taxonomyParser.js';
 
 const debug = Debug('syncTaxonomy');
 
 export async function sync(connection) {
-  const lastFile = await getLastTaxonomyFile();
-  const progress = await connection.getProgress('taxonomies');
-  const collection = await connection.getCollection('taxonomies');
-
-  const lastDocumentImported = await getLastTaxonomyImported(
+  let options = {
+    collectionSource: process.env.TAXONOMY_SOURCE,
+    destinationLocal: `${process.env.ORIGINAL_DATA_PATH}/taxonomy/full`,
+    collectionName: 'taxonomies',
+    filenameNew: 'taxonomy',
+    extensionNew: 'zip',
+  };
+  const lastFile = await getLastFileSync(options);
+  const sources = [lastFile.replace(process.env.ORIGINAL_DATA_PATH, '')];
+  const progress = await connection.getProgress(options.collectionName);
+  const collection = await connection.getCollection(options.collectionName);
+  const logs = await connection.geImportationtLog({
+    collectionName: options.collectionName,
+    sources,
+    startSequenceID: progress.seq,
+  });
+  const lastDocumentImported = await getLastDocumentImported(
     connection,
     progress,
+    options.collectionName,
   );
   let firstID;
-  if (
-    lastDocumentImported &&
-    lastDocumentImported._source &&
-    lastFile.includes(lastDocumentImported._source)
-  ) {
+  if (lastDocumentImported !== null) {
     firstID = lastDocumentImported._id;
   }
+
   const fileList = (await fileListFromZip(readFileSync(lastFile))).filter(
     (file) => file.name === 'fullnamelineage.dmp',
   );
   const arrayBuffer = await fileList[0].arrayBuffer();
 
   // we reparse all the file and skip if required
-  const source = lastFile.replace(process.env.ORIGINAL_DATA_PATH, '');
   let skipping = firstID !== undefined;
   let counter = 0;
   let imported = 0;
   let start = Date.now();
   if (
     lastDocumentImported === null ||
-    (!lastFile.includes(lastDocumentImported._source) &&
-      progress.state === 'updated') ||
+    md5(JSON.stringify(sources)) !== progress.sources ||
     progress.state !== 'updated'
   ) {
-    progress.state = 'updating';
-    await connection.setProgress(progress);
     for (const entry of taxonomyParser(arrayBuffer)) {
       counter++;
       if (process.env.TEST === 'true' && counter > 20) break;
@@ -59,8 +64,8 @@ export async function sync(connection) {
         }
         continue;
       }
+      progress.state = 'updating';
       entry._seq = ++progress.seq;
-      entry._source = source;
       await collection.updateOne(
         { _id: entry._id },
         { $set: entry },
@@ -70,38 +75,22 @@ export async function sync(connection) {
       await connection.setProgress(progress);
       imported++;
     }
+    logs.dateEnd = Date.now();
+    logs.endSequenceID = progress.seq;
+    logs.status = 'updated';
+    await connection.updateImportationLog(logs);
+    progress.sources = md5(JSON.stringify(sources));
+    progress.date = new Date();
+    progress.state = 'updated';
+    await connection.setProgress(progress);
+    debug(`${imported} taxonomies processed`);
+  } else {
+    debug(`file already processed`);
   }
-  progress.state = 'updated';
-  await connection.setProgress(progress);
-
-  debug(`${imported} taxonomies processed`);
 
   // we remove all the entries that are not imported by the last file
   const result = await collection.deleteMany({
-    _source: { $ne: source },
+    _seq: { $lte: logs.startSequenceID },
   });
   debug(`Deleting entries with wrong source: ${result.deletedCount}`);
-}
-
-async function getLastTaxonomyImported(connection, progress) {
-  const collection = await connection.getCollection('taxonomies');
-  return collection
-    .find({ _seq: { $lte: progress.seq } })
-    .sort('_seq', -1)
-    .limit(1)
-    .next();
-}
-
-async function getLastTaxonomyFile() {
-  debug('Get last taxonomy file if new');
-
-  const source = process.env.TAXONOMY_SOURCE;
-  const destination = `${process.env.ORIGINAL_DATA_PATH}/taxonomy/full`;
-
-  debug(`Syncing: ${source} to ${destination}`);
-
-  return getFileIfNew({ url: source }, destination, {
-    filename: 'taxonomy',
-    extension: 'zip',
-  });
 }
