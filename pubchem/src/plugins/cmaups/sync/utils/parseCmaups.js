c09fa3335170a602750aa4c50b71445f13cc3773
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-04-29 12:22:27 +0200

    fix: an nps can have more than one taxonomy, the code was just using the first match

--- pubchem/src/plugins/cmaups/sync/utils/parseCmaups.js
+++ pubchem/src/plugins/cmaups/sync/utils/parseCmaups.js
@@ -3,7 +3,6 @@ import OCL from 'openchemlib';
 import Debug from '../../../../utils/Debug.js';
 
 const debug = Debug('parseCmaups');
-
 export async function* parseCmaups(
   general,
   activities,
@@ -16,7 +15,10 @@ export async function* parseCmaups(
     const speciesPaired = {};
 
     for (const pair of speciesPair) {
-      speciesPaired[pair[1]] = pair[0];
+      if (!speciesPaired[pair[1]]) {
+        speciesPaired[pair[1]] = [];
+      }
+      speciesPaired[pair[1]].push(pair[0]);
     }
     let errorsCounter = 0;
     let skipping = true;
@@ -32,6 +34,7 @@ export async function* parseCmaups(
         if (Object.keys(item.Ingredient_ID).length > 0) {
           const id = item.Ingredient_ID;
           const activity = activities[id];
+
           const finalActivities = [];
           if (activity !== undefined) {
             for (const info of activity) {
@@ -44,6 +47,7 @@ export async function* parseCmaups(
               });
             }
           }
+
           const smilesDb = item.__parsed_extra.slice(-1)[0];
           let oclID;
           let noStereoID;
@@ -64,34 +68,49 @@ export async function* parseCmaups(
 
             continue;
           }
-          const orgID = speciesPaired[id];
-          const taxonomies = speciesInfo[orgID];
-          let finalTaxonomies = [];
-          if (taxonomies !== undefined) {
-            let taxons = {};
-            if (taxonomies?.Species_Tax_ID) {
-              taxons.speciesID;
-            }
-            if (taxonomies?.Plant_Name) {
-              taxons.species = taxonomies?.Plant_Name;
-            }
-            if (taxonomies?.Genus_Tax_ID) {
-              taxons.genusID = taxonomies?.Genus_Tax_ID;
-            }
-            if (taxonomies?.Genus_Name) {
-              taxons.genus = taxonomies?.Genus_Name;
-            }
 
-            if (taxonomies?.Family_Tax_ID) {
-              taxons.familyID = taxonomies?.Family_Tax_ID;
-            }
-            if (taxonomies?.Family_Name) {
-              taxons.family = taxonomies?.Family_Name;
-            }
-            if (taxons !== {}) {
-              finalTaxonomies.push(taxons);
+          const orgIDs = speciesPaired[id];
+
+          const taxonomies = [];
+          if (orgIDs) {
+            if (orgIDs.length > 0) {
+              orgIDs.forEach((idOrg) => {
+                if (speciesInfo[idOrg]) {
+                  taxonomies.push(speciesInfo[idOrg]);
+                }
+              });
             }
           }
+
+          let finalTaxonomies = [];
+          if (taxonomies.length > 0) {
+            for (const infos of taxonomies) {
+              let taxons = {};
+              if (infos?.Species_Tax_ID) {
+                taxons.speciesID = infos.Species_Tax_ID;
+              }
+              if (infos?.Plant_Name) {
+                taxons.species = infos?.Plant_Name;
+              }
+              if (infos?.Genus_Tax_ID) {
+                taxons.genusID = infos?.Genus_Tax_ID;
+              }
+              if (infos?.Genus_Name) {
+                taxons.genus = infos?.Genus_Name;
+              }
+
+              if (infos?.Family_Tax_ID) {
+                taxons.familyID = infos?.Family_Tax_ID;
+              }
+              if (infos?.Family_Name) {
+                taxons.family = infos?.Family_Name;
+              }
+              if (taxons !== {}) {
+                finalTaxonomies.push(taxons);
+              }
+            }
+          }
+
           const result = {
             _id: item.Ingredient_ID,
             data: {
@@ -103,18 +122,20 @@ export async function* parseCmaups(
             },
           };
           if (item.pubchem_cid) result.data.cid = item.pubchem_cid;
-          if (finalTaxonomies.length !== 0) {
+          if (finalTaxonomies.length > 0) {
             result.data.taxonomies = finalTaxonomies;
           }
-          if (finalActivities.length !== 0) {
+          if (finalActivities.length > 0) {
             result.data.activities = finalActivities;
           }
+
           yield result;
         }
       } catch (e) {
         continue;
       }
     }
+
     debug(
       `Class$S16: Assignment of aromatic double bonds failed --> count: ${errorsCounter}`,
     );
