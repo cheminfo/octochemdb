9c450795cae854a67d72b4cddf9d6c0c05c9c815
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-05-10 14:04:30 +0200

    feat: limit number of logs showed

--- pubchem/src/plugins/importationLogs/routes/v1/entriesImportationLogs.js
+++ pubchem/src/plugins/importationLogs/routes/v1/entriesImportationLogs.js
@@ -4,52 +4,66 @@ import Debug from '../../../../utils/Debug.js';
 
 const debug = Debug('entries importation Logs');
 
-const entriesAdmin = {
+const entriesImportationLogs = {
   method: 'GET',
   schema: {
     querystring: {
-      collectionToSearch: {
+      collectionName: {
         type: 'string',
-        description: 'Collection progress',
+        description: '"Collection Name"',
         example: 'bioassays',
         default: 'bioassays',
+      },
+      limit: {
+        type: 'number',
+        description: '"Max result output"',
+        example: 10,
+        default: 50,
       },
       fields: {
         type: 'string',
         description: 'Fields to retrieve',
-        default: 'state,seq,date,sources,logs',
+        default:
+          'sources,dateStart,dateEnd,startSequenceID,endSequenceID,status',
       },
     },
   },
   handler: searchHandler,
 };
 
-export default entriesAdmin;
+export default entriesImportationLogs;
 
 async function searchHandler(request) {
-  let { collectionToSearch = '', fields = 'state,seq,date,sources,logs' } =
-    request.query;
+  let {
+    collectionName = '',
+    limit = 0,
+    fields = 'sources,dateStart,dateEnd,startSequenceID,endSequenceID,status',
+  } = request.query;
 
   let connection;
   try {
     connection = new PubChemConnection();
-    const collection = await connection.getCollection('admin');
+    const collection = await connection.getCollection('importationLogs');
 
-    debug(JSON.stringify({ collectionToSearch }));
+    debug(JSON.stringify({ collectionName }));
     let formatedFields = getFields(fields);
     formatedFields._id = 0;
     const results = await collection
       .aggregate([
         {
           $match: {
-            _id: `${collectionToSearch}_progress`,
+            collectionName,
           },
         },
+        { $limit: Number(limit) },
         {
           $project: formatedFields,
         },
       ])
       .toArray();
+    debug({
+      collectionName: collectionName,
+    });
     return results;
   } catch (e) {
     debug(e.stack);
