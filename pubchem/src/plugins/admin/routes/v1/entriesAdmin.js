9f8052ed2cfc70104a7ca64be1bdb4930e5350be
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-05-10 14:04:03 +0200

    feat: set limit of progress logs message

--- pubchem/src/plugins/admin/routes/v1/entriesAdmin.js
+++ pubchem/src/plugins/admin/routes/v1/entriesAdmin.js
@@ -14,6 +14,12 @@ const entriesAdmin = {
         example: 'bioassays',
         default: 'bioassays',
       },
+      limit: {
+        type: 'number',
+        description: 'limit logs message',
+        example: 10,
+        default: 50,
+      },
       fields: {
         type: 'string',
         description: 'Fields to retrieve',
@@ -27,8 +33,11 @@ const entriesAdmin = {
 export default entriesAdmin;
 
 async function searchHandler(request) {
-  let { collectionToSearch = '', fields = 'state,seq,date,sources,logs' } =
-    request.query;
+  let {
+    collectionToSearch = '',
+    limit = 0,
+    fields = 'state,seq,date,sources,logs',
+  } = request.query;
 
   let connection;
   try {
@@ -38,6 +47,7 @@ async function searchHandler(request) {
     debug(JSON.stringify({ collectionToSearch }));
     let formatedFields = getFields(fields);
     formatedFields._id = 0;
+    formatedFields.logs = { $slice: ['$logs', Number(limit)] };
     const results = await collection
       .aggregate([
         {
@@ -50,6 +60,7 @@ async function searchHandler(request) {
         },
       ])
       .toArray();
+
     return results;
   } catch (e) {
     debug(e.stack);
