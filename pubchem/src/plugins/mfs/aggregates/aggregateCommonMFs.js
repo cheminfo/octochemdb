eef7f310331f72fe0ece2e2b8d809dcca463f805
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-05-16 12:54:19 +0200

    test: limit at 1e7 entries the aggregation

--- pubchem/src/plugins/mfs/aggregates/aggregateCommonMFs.js
+++ pubchem/src/plugins/mfs/aggregates/aggregateCommonMFs.js
@@ -12,15 +12,12 @@ export async function aggregate(connection) {
     return;
   }
 
-  debug(
-    'mfsCommon: Need to aggregate',
-    await collection.countDocuments(),
-    'entries',
-  );
+  debug('mfsCommon: Need to aggregate', await collection.countDocuments());
   let result = await collection.aggregate(
     [
-      //    { $limit: 1e4 },
+      //
       { $match: { 'data.nbFragments': 1, 'data.charge': 0 } }, // we don't want charges in MF
+      { $limit: 1e7 },
       {
         $project: {
           _id: 0,
