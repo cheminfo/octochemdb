23c855617cc6a9d13b29a41808e6a770e9eb54cd
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-05-18 16:46:18 +0200

    feat: search by EM or taxonomy (even if incomplete) or both

--- pubchem/src/plugins/compoundsAggregation/routes/v1/aggregationFromEM.js
+++ pubchem/src/plugins/compoundsAggregation/routes/v1/aggregationFromEM.js
@@ -17,6 +17,26 @@ const entriesFromEM = {
         example: 300.123,
         default: null,
       },
+
+      familyName: {
+        type: 'string',
+        description: 'genus',
+        example: 'Podocarpus',
+        default: 'Podocarpus',
+      },
+      genusName: {
+        type: 'string',
+        description: 'genus',
+        example: 'Podocarpus',
+        default: 'Podocarpus',
+      },
+      speciesName: {
+        type: 'string',
+        description: 'species',
+        example: 'Podocarpus macrophyllus or macrophyllus',
+        default: null,
+      },
+
       precision: {
         type: 'number',
         description: 'Precision (in ppm) of the monoisotopic mass',
@@ -31,7 +51,7 @@ const entriesFromEM = {
         type: 'string',
         description: 'Fields to retrieve',
         default:
-          'data.em,data.charge,data.unsaturation,data.active,data.ocls,data.names,data.keywords,data.activities,data.taxonomies',
+          'data.em,data.mf,data.charge,data.unsaturation,data.active,data.naturalProduct,data.ocls,data.names,data.keywords,data.activities,data.taxonomies',
       },
     },
   },
@@ -43,23 +63,67 @@ export default entriesFromEM;
 async function searchHandler(request) {
   let {
     em = 0,
+    familyName = '',
+    genusName = '',
+    speciesName = '',
     limit = 1e3,
     precision = 100,
-    fields = 'data.em,data.charge,data.unsaturation,data.active,data.ocls,data.names,data.keywords,data.activities,data.taxonomies',
+
+    fields = 'data.em,data.mf,data.charge,data.unsaturation,data.active,data.naturalProduct,data.ocls,data.names,data.keywords,data.activities,data.taxonomies',
   } = request.query;
 
   if (limit > 1e4) limit = 1e4;
   if (limit < 1) limit = 1;
   let error = (em / 1e6) * precision;
 
+  let family = familyName.toLowerCase();
+  let genus = genusName.toLowerCase();
+  let species;
+  if (speciesName.split(' ').length > 1) {
+    species = speciesName.split(' ')[1].toLowerCase();
+  } else {
+    species = speciesName.toLowerCase();
+  }
+  let searchTaxonomyParameter;
+  let alreadySelected = false;
+  if (species !== '') {
+    searchTaxonomyParameter = species;
+    alreadySelected = true;
+  }
+  if (genus !== '' && !alreadySelected) {
+    searchTaxonomyParameter = genus;
+    alreadySelected = true;
+  }
+  if (family !== '' && !alreadySelected) {
+    searchTaxonomyParameter = family;
+    alreadySelected = true;
+  }
+
   let connection;
+
   try {
     connection = new PubChemConnection();
-    const collection = await connection.getCollection('bestOfCompounds');
+    const collection = await connection.getCollection('temporaryAgregation');
     let formatedFields = getFields(fields);
     formatedFields._id = 0;
     debug(em);
-
+    let matchParameter;
+    if (em !== 0 && searchTaxonomyParameter !== '') {
+      matchParameter = {
+        'data.em': { $lt: em + error, $gt: em - error },
+        'data.keywords': { $in: [new RegExp(searchTaxonomyParameter, 'i')] },
+      };
+    }
+    if (em === 0 && searchTaxonomyParameter !== '') {
+      matchParameter = {
+        'data.keywords': { $in: [new RegExp(searchTaxonomyParameter, 'i')] },
+      };
+    }
+    if (em !== 0 && searchTaxonomyParameter === '') {
+      matchParameter = {
+        'data.em': { $lt: em + error, $gt: em - error },
+      };
+    }
     const results = await collection
       .aggregate([
         { $match: { 'data.em': { $lt: em + error, $gt: em - error } } },
