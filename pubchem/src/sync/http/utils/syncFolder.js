a2f8f21f9fa2cea5f0d38e41e543104d2b8eccef
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-05-19 10:43:10 +0200

    fix: download of killed IDs

--- pubchem/src/sync/http/utils/syncFolder.js
+++ pubchem/src/sync/http/utils/syncFolder.js
@@ -37,12 +37,8 @@ async function syncFolder(source, destinationFolder, options = {}) {
     const targetFile = join(destinationFolder, file.name);
     file.path = targetFile;
     if (skipping) {
-      if (
-        file.name !== lastFileImported.name &&
-        fileList !== [] &&
-        !file.name.includes('killed-CIDs')
-      ) {
-        if (!file.name.includes('killed-CIDs')) {
+      if (file.name !== lastFileImported.name && fileList !== []) {
+        if (!file.name.includes('killed')) {
           continue;
         }
       } else {
