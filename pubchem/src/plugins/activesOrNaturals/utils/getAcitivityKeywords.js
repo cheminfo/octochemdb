b7782b7615a0724f2ce113914d28c71f85a92a68
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-05-30 10:34:13 +0200

    chore: keep superkingom, kingdom and phylum of assay organism

--- pubchem/src/plugins/activesOrNaturals/utils/getAcitivityKeywords.js
+++ pubchem/src/plugins/activesOrNaturals/utils/getAcitivityKeywords.js
@@ -3,7 +3,13 @@ import { isIgnoreKeyword } from '../../../utils/isIgnoreKeyword.js';
 export function getActivityKeywords(activities) {
   const activitesKW = new Set();
   for (let activity of activities) {
-    const keywords = activity.assay
+    const keywords = [
+      activity.assay,
+      activity.taxonomy.superkingdom,
+      activity.taxonomy.kingdom,
+      activity.taxonomy.phylum,
+    ]
+      .join(' ')
       .toLowerCase()
       .split(/\W+/)
       .filter((k) => k);
