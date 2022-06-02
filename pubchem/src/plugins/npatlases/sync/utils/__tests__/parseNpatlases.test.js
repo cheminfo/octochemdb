bb7b1d4daf11192dc3b85ab8ad359e23d289409a
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-06-02 10:47:51 +0200

    chore: test case parseNpatlases

--- pubchem/src/plugins/npatlases/sync/utils/__tests__/parseNpatlases.test.js
+++ pubchem/src/plugins/npatlases/sync/utils/__tests__/parseNpatlases.test.js
@@ -3,30 +3,35 @@ import { join } from 'path';
 
 import { parseNpatlases } from '../parseNpatlases.js';
 
-test('parseNpatlases', () => {
+test('parseNpatlases', async () => {
   const json = readFileSync(
     join(__dirname, 'data/NPAtlas_download.json'),
     'utf8',
   );
-  const results = parseNpatlases(JSON.parse(json));
+
+  let results = [];
+  for await (const entry of parseNpatlases(JSON.parse(json))) {
+    results.push(entry);
+  }
   expect(results[0]).toStrictEqual({
     _id: 'NPA000001',
     data: {
       ocl: {
         id: 'ficaP@D@tZK\\bbRbTRdQT\\VfZfjjjjjjXRLPiZIUU@d@',
-        coordinates:
-          '!B?g~w_Xa}mpJH@k]}?`BH_[\\B?g~H?Oy?b@Jw?[]}bGw~@Ha}b@K~_xa}?g~H@ha}',
         noStereoID: 'ficaP@D@tZK\\bbRbTRdQT\\VfZfjjjjjj@@',
       },
       taxonomies: [
         {
-          doi: '10.1002/chem.201000652',
           genusID: 5502,
           genus: 'Curvularia',
           species: 'Curvularia geniculata',
+          kingdom: 'Fungi',
+          phylum: 'Ascomycota',
+          class: 'Dothideomycetes',
+          family: 'Pleosporaceae',
+          doi: '10.1002/chem.201000652',
         },
       ],
-
       iupacName: 'Curvularide C',
     },
   });
