cc5d2c6bb4e6e3ab97efe31188a70c7798767a46
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-06-07 15:11:25 +0200

    test: cid

--- pubchem/src/plugins/naturalSubstances/aggregates/aggregateNaturalSubstances.js
+++ pubchem/src/plugins/naturalSubstances/aggregates/aggregateNaturalSubstances.js
@@ -64,7 +64,7 @@ export async function aggregate(connection) {
         });
 
         if (!compound) {
-          debug(compound);
+          debug(cid);
           continue;
         }
         let noStereoID = compound.data.ocl.noStereoID;
