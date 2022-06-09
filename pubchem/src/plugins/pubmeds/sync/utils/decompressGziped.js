8d5fa323c0d4bed6ae82cf1113c49bea236cda86
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-06-09 16:59:26 +0200

    feat: import pubmeds

---
+++ pubchem/src/plugins/pubmeds/sync/utils/decompressGziped.js
@@ -0,0 +1,37 @@
+/**
+ * decompress a gziped file and return the path to the unzipped file with the same name
+ * @param {string} inputFilename path to the gziped file
+ * @returns {string} path to the unzipped file
+ */
+
+import { createReadStream, createWriteStream } from 'fs';
+import { join } from 'path';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
+import { createGunzip } from 'zlib';
+
+import pkg from 'fs-extra';
+
+import Debug from '../../../../utils/Debug.js';
+
+const { existsSync } = pkg;
+
+const pipe = promisify(pipeline);
+const debug = Debug('extractGziped');
+
+export async function decompressGziped(inputFilename) {
+  const outputFilename = inputFilename.replace('.gz', '');
+  if (!existsSync(outputFilename)) {
+    debug(`decompress ${inputFilename}`);
+    const gzip = createGunzip();
+    const source = createReadStream(inputFilename);
+    const destination = createWriteStream(join(`${outputFilename}`));
+    await pipe(source, gzip, destination).catch(() => {
+      process.exitCode = 1;
+    });
+  } else {
+    debug(`${outputFilename} already exists`);
+  }
+
+  return join(`${outputFilename}`);
+}
--- pubchem/src/plugins/pubmeds/sync/utils/importOnePubmedFile.js
+++ pubchem/src/plugins/pubmeds/sync/utils/importOnePubmedFile.js
@@ -1,12 +1,11 @@
-import fs from 'fs';
+import { rmSync } from 'fs';
 import { open } from 'fs/promises';
-import { join } from 'path';
-import { createGunzip } from 'zlib';
 
 import { parseStream } from 'arraybuffer-xml-parser';
 
 import Debug from '../../../../utils/Debug.js';
 
+import { decompressGziped } from './decompressGziped.js';
 import improvePubmedPool from './improvePubmedPool.js';
 
 const debug = Debug('importOnePubmedFile');
@@ -17,70 +16,53 @@ export default async function importOnePubmedFile(
   file,
   options,
 ) {
-  const collection = await connection.getCollection('pubmeds');
-  await collection.createIndex({ _seq: 1 });
-  debug(`Importing: ${file.name}`);
-  const logs = await connection.geImportationLog({
-    collectionName: 'pubmeds',
-    sources: file.name,
-    startSequenceID: progress.seq,
-  });
-
-  // unzip gzip file and return path to the unzipped file
-  const unzipFile = async (filePath) => {
-    const gunzipStream = createGunzip();
-    const readStream = fs.createReadStream(filePath);
-    readStream.pipe(gunzipStream);
-    const unzippedFilePath = filePath.replace('.gz', '');
-    const writeStream = fs.createWriteStream(unzippedFilePath);
-    gunzipStream.pipe(writeStream);
-    await writeStream.on('finish', () => {
-      debug(`Unzipped ${filePath} to ${unzippedFilePath}`);
+  try {
+    const collection = await connection.getCollection('pubmeds');
+    await collection.createIndex({ _seq: 1 });
+    debug(`Importing: ${file.name}`);
+    const logs = await connection.geImportationLog({
+      collectionName: 'pubmeds',
+      sources: file.name,
+      startSequenceID: progress.seq,
     });
-    return unzippedFilePath;
-  };
-  const path = await unzipFile(file.path);
-  const files = await open(join(path), 'r');
-  const stream = files.readableWebStream();
 
-  // parse the xml file
-  // let i = 0;
-
-  //  const fileStream = await open(file.path, 'r');
-  //const readableStream = fileStream.readableWebStream();
-  // debug(readableStream);
-  let { shouldImport, lastDocument } = options;
-  let imported = 0;
-  const actions = [];
-  for await (const entry of parseStream(stream, 'PubmedArticle')) {
-    if (!shouldImport) {
-      if (entry.MedlineCitation.PMID['#text'] !== lastDocument._id) {
+    const filePath = await decompressGziped(file.path);
+    const fileStream = await open(filePath, 'r');
+    const readableStream = fileStream.readableWebStream();
+    let { shouldImport, lastDocument } = options;
+    let imported = 0;
+    const actions = [];
+    debug(`Importing ${file.name}`);
+    for await (const entry of parseStream(readableStream, 'PubmedArticle')) {
+      if (!shouldImport) {
+        if (entry.MedlineCitation.PMID['#text'] !== lastDocument._id) {
+          continue;
+        }
+        shouldImport = true;
+        debug(`Skipping pubmeds till: ${lastDocument._id}`);
         continue;
       }
-      shouldImport = true;
-      debug(`Skipping pubmeds till: ${lastDocument._id}`);
-      continue;
-    }
-    if (shouldImport) {
-      actions.push(
-        improvePubmedPool(entry)
-          .then((result) => {
-            result._seq = ++progress.seq;
-            return collection.updateOne(
-              { _id: result._id },
-              { $set: result },
-              { upsert: true },
-            );
-          })
-          .then(() => {
-            progress.sources = file.path.replace(
-              process.env.ORIGINAL_DATA_PATH,
-              '',
-            );
-            return connection.setProgress(progress);
-          }),
-      );
-      imported++;
+      if (shouldImport) {
+        actions.push(
+          improvePubmedPool(entry)
+            .then((result) => {
+              result._seq = ++progress.seq;
+              return collection.updateOne(
+                { _id: result._id },
+                { $set: result },
+                { upsert: true },
+              );
+            })
+            .then(() => {
+              progress.sources = file.path.replace(
+                process.env.ORIGINAL_DATA_PATH,
+                '',
+              );
+              return connection.setProgress(progress);
+            }),
+        );
+        imported++;
+      }
     }
     await Promise.all(actions);
     logs.dateEnd = Date.now();
@@ -88,7 +70,11 @@ export default async function importOnePubmedFile(
     logs.status = 'updated';
     await connection.updateImportationLog(logs);
     debug(`${imported} articles processed`);
+    // Remove the decompressed gzip file after it has been imported
+    await fileStream.close();
+    rmSync(filePath, { recursive: true });
+    return imported;
+  } catch (err) {
+    debug(err, { collection: 'pubmeds', connection });
   }
-
-  return imported;
 }
