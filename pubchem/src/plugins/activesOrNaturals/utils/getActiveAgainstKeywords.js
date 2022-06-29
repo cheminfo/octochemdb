539a83fdc8c4ee2f38821d66d64fa8189c74d367
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-06-29 11:16:40 +0200

    fix: targetTaxonomies is an array

--- pubchem/src/plugins/activesOrNaturals/utils/getActiveAgainstKeywords.js
+++ pubchem/src/plugins/activesOrNaturals/utils/getActiveAgainstKeywords.js
@@ -7,26 +7,28 @@ export default function getActiveAgainstKeywords(activities) {
   const activeAgainstKw = new Set();
   for (let activity of activities) {
     if (activity.targetTaxonomies) {
-      const keywordsSuperKingdom = [activity.targetTaxonomies.superkingdom]
-        .join(' ')
-        .toLowerCase()
-        .split(/\W+/);
-      const keywordsKingdom = [activity.targetTaxonomies.kingdom]
-        .join(' ')
-        .toLowerCase()
-        .split(/\W+/);
-      const keywordsPhylum = [activity.targetTaxonomies.phylum]
-        .join(' ')
-        .toLowerCase()
-        .split(/\W+/);
-      const keywords = [
-        ...keywordsSuperKingdom,
-        ...keywordsKingdom,
-        ...keywordsPhylum,
-      ];
-      for (let keyword of keywords) {
-        if (keyword !== '') {
-          activeAgainstKw.add(keyword);
+      for (let taxonomy of activity.targetTaxonomies) {
+        const keywordsSuperKingdom = [taxonomy.superkingdom]
+          .join(' ')
+          .toLowerCase()
+          .split(/\W+/);
+        const keywordsKingdom = [taxonomy.kingdom]
+          .join(' ')
+          .toLowerCase()
+          .split(/\W+/);
+        const keywordsPhylum = [taxonomy.phylum]
+          .join(' ')
+          .toLowerCase()
+          .split(/\W+/);
+        const keywords = [
+          ...keywordsSuperKingdom,
+          ...keywordsKingdom,
+          ...keywordsPhylum,
+        ];
+        for (let keyword of keywords) {
+          if (keyword !== '') {
+            activeAgainstKw.add(keyword);
+          }
         }
       }
     }
