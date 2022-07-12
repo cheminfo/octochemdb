1ef0ac3485afb9a353215691cd59a81d8d610e52
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-07-12 09:18:39 +0200

    wip: changed try and catch

--- pubchem/src/plugins/compounds/sync/utils/importOneCompoundFile.js
+++ pubchem/src/plugins/compounds/sync/utils/importOneCompoundFile.js
@@ -22,21 +22,21 @@ export default async function importOneCompoundFile(
   file,
   options,
 ) {
+  // get compounds collection
+  const collection = await connection.getCollection('compounds');
+  debug(`Importing: ${file.name}`);
+  // Get logs collection
+  const logs = await connection.getImportationLog({
+    collectionName: 'compounds',
+    sources: file.name,
+    startSequenceID: progress.seq,
+  });
+  // should we directly import the data or wait that we reach the previously imported information
+  let { shouldImport = true, lastDocument } = options;
+  // Create a readStream for the file
+  let bufferValue = '';
+  let newCompounds = 0;
   try {
-    // get compounds collection
-    const collection = await connection.getCollection('compounds');
-    debug(`Importing: ${file.name}`);
-    // Get logs collection
-    const logs = await connection.getImportationLog({
-      collectionName: 'compounds',
-      sources: file.name,
-      startSequenceID: progress.seq,
-    });
-    // should we directly import the data or wait that we reach the previously imported information
-    let { shouldImport = true, lastDocument } = options;
-    // Create a readStream for the file
-    let bufferValue = '';
-    let newCompounds = 0;
     const readStream = fs.createReadStream(file.path);
     const unzipStream = readStream.pipe(zlib.createGunzip());
     for await (const chunk of unzipStream) {
@@ -58,62 +58,62 @@ export default async function importOneCompoundFile(
     debug(`${newCompounds} compounds imported from ${file.name}`);
     // return the new compounds count
     return newCompounds;
-
-    // parse the SDF file (function called in line 50) and import the compounds in the compounds collection
-    async function parseSDF(sdf) {
-      // parse the SDF file
-      let compounds = parse(sdf).molecules;
-      debug(`Need to process ${compounds.length} compounds`);
-      // if test mode is enabled, we only process the first 10 compounds
-      if (process.env.TEST === 'true') compounds = compounds.slice(0, 10);
-      // the array action will contain the promises to be resolved
-      const actions = [];
-      let start = Date.now();
-      for (const compound of compounds) {
-        // skip till CID corresponds to the last document imported ID
-        if (!shouldImport) {
-          if (compound.PUBCHEM_COMPOUND_CID !== lastDocument._id) {
-            continue;
-          }
-          shouldImport = true;
-          if (
-            Date.now() - start >
-            Number(process.env.DEBUG_THROTTLING || 10000)
-          ) {
-            debug(`Skipping compounds till: ${lastDocument._id}`);
-            start = Date.now();
-            continue;
-          }
-        }
-        // promises to be resolved
-        actions.push(
-          improveCompoundPool(compound)
-            .then((result) => {
-              result._seq = ++progress.seq;
-              return collection.updateOne(
-                { _id: result._id },
-                { $set: result },
-                { upsert: true },
-              );
-            })
-            .then(() => {
-              progress.sources = file.path.replace(
-                process.env.ORIGINAL_DATA_PATH,
-                '',
-              );
-              return connection.setProgress(progress);
-            }),
-        );
-      }
-      newCompounds += actions.length;
-      // wait for all the promises to be resolved
-      await Promise.all(actions);
-      debug(`${newCompounds} compounds processed`);
-      return compounds.length;
-    }
   } catch (e) {
     if (connection) {
       debug(e.message, { collection: 'compounds', connection, stack: e.stack });
     }
+  }
+
+  // parse the SDF file (function called in line 50) and import the compounds in the compounds collection
+  async function parseSDF(sdf) {
+    // parse the SDF file
+    let compounds = parse(sdf).molecules;
+    debug(`Need to process ${compounds.length} compounds`);
+    // if test mode is enabled, we only process the first 10 compounds
+    if (process.env.TEST === 'true') compounds = compounds.slice(0, 10);
+    // the array action will contain the promises to be resolved
+    const actions = [];
+    let start = Date.now();
+    for (const compound of compounds) {
+      // skip till CID corresponds to the last document imported ID
+      if (!shouldImport) {
+        if (compound.PUBCHEM_COMPOUND_CID !== lastDocument._id) {
+          continue;
+        }
+        shouldImport = true;
+        if (
+          Date.now() - start >
+          Number(process.env.DEBUG_THROTTLING || 10000)
+        ) {
+          debug(`Skipping compounds till: ${lastDocument._id}`);
+          start = Date.now();
+          continue;
+        }
+      }
+      // promises to be resolved
+      actions.push(
+        improveCompoundPool(compound)
+          .then((result) => {
+            result._seq = ++progress.seq;
+            return collection.updateOne(
+              { _id: result._id },
+              { $set: result },
+              { upsert: true },
+            );
+          })
+          .then(() => {
+            progress.sources = file.path.replace(
+              process.env.ORIGINAL_DATA_PATH,
+              '',
+            );
+            return connection.setProgress(progress);
+          }),
+      );
+    }
+    newCompounds += actions.length;
+    // wait for all the promises to be resolved
+    await Promise.all(actions);
+    debug(`${newCompounds} compounds processed`);
+    return compounds.length;
   }
 }
