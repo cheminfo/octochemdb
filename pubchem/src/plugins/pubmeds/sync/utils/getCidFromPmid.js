5012147ebed3bbfc68f821f95aa8854e97934d1b
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-07-19 16:25:56 +0200

    fix: keep only first 1000 pmids

--- pubchem/src/plugins/pubmeds/sync/utils/getCidFromPmid.js
+++ pubchem/src/plugins/pubmeds/sync/utils/getCidFromPmid.js
@@ -38,7 +38,7 @@ export async function getCidFromPmid(filePath, connection) {
       }
     }
     for (let key in data) {
-      if (data[key].length > 500) {
+      if (data[key].length > 1000) {
         data[key].length = 0;
       }
     }
