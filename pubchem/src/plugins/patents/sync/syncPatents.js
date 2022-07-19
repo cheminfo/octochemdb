32c4873446b3e709036e64e9bd378b818f7dc8b4
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-07-19 13:15:16 +0200

    wip: test importation

--- pubchem/src/plugins/patents/sync/syncPatents.js
+++ pubchem/src/plugins/patents/sync/syncPatents.js
@@ -1,17 +1,12 @@
-import { join } from 'path';
-
 import pkg from 'fs-extra';
 
 import getLastFileSync from '../../../sync/http/utils/getLastFileSync.js';
 import Debug from '../../../utils/Debug.js';
 
-import calculateDiff from './utils/calculateDiff.js';
 import firstPatentsImport from './utils/firstPatentsImport.js';
-import { getLatestsImportedFile } from './utils/getLatestsImportedFile.js';
-import parsePatents from './utils/parsePatents.js';
 import ungzipAndSort from './utils/ungzipAndSort.js';
 
-const { removeSync, existsSync, unlinkSync, linkSync } = pkg;
+const { removeSync } = pkg;
 /**
  * @description sync patents from PubChem database
  * @param {*} connection - mongo connection
@@ -27,60 +22,23 @@ export async function sync(connection) {
       filenameNew: 'cidToPatents',
       extensionNew: 'gz',
     };
-    const collection = await connection.getCollection(options.collectionName);
 
     // get last files cidToPatens available in the PubChem database
-    const lastFile = await getLastFileSync(options);
+    // const lastFile = await getLastFileSync(options);
     //sort file by cid
-    const sortedFile = `${lastFile.split('.gz')[0]}.sorted`;
-    await ungzipAndSort(lastFile, sortedFile);
+    //const sortedFile = `${lastFile.split('.gz')[0]}.sorted`;
+    //await ungzipAndSort(lastFile, sortedFile);
     //  remove non-sorted file
-    removeSync(lastFile);
-    const lastImportedFilePath = await getLatestsImportedFile(
-      `${options.destinationLocal}/old`,
-      'cidToPatents',
-    );
-    if (lastImportedFilePath === null) {
-      const filenames = await createDiffFromLastTwoFiles(
-        lastImportedFilePath,
-        sortedFile,
-      );
-      let success = await parsePatents(
-        filenames.diffFile,
-        collection,
-        connection,
-      );
-      if (success) {
-        if (existsSync(filenames.previousFile)) {
-          unlinkSync(filenames.previousFile);
-        }
-        linkSync(filenames.lastFile, filenames.previousFile);
-      }
-    } else {
-      await firstPatentsImport(sortedFile, collection, connection);
-    }
+    // removeSync(lastFile);
+    const sortedFile = `${options.destinationLocal}/cidToPatents.2022-07-14.sorted`;
+    await firstPatentsImport(sortedFile, connection);
+    const collection = await connection.getCollection(options.collectionName);
     await collection.createIndex({ _id: 1 });
-    await collection.createIndex({ data: 1 });
+    await collection.createIndex({ 'data.patents': 1 });
+    await collection.createIndex({ 'data.nbPatents': 1 });
   } catch (e) {
     if (connection) {
       debug(e.message, { collection: 'patents', connection, stack: e.stack });
     }
   }
-}
-
-async function createDiffFromLastTwoFiles(newFileToImport, lastImportedFile) {
-  const lastFile = join(newFileToImport);
-  let previousFile = join(lastImportedFile);
-  let currentPreviousFile = existsSync(previousFile)
-    ? previousFile
-    : '/dev/null';
-
-  let diffFile = lastFile.replace('.sorted', '.diff');
-  await calculateDiff(currentPreviousFile, lastFile, diffFile);
-  return {
-    currentPreviousFile,
-    lastFile,
-    previousFile,
-    diffFile,
-  };
 }
--- pubchem/src/plugins/patents/sync/utils/firstPatentsImport.js
+++ pubchem/src/plugins/patents/sync/utils/firstPatentsImport.js
@@ -4,45 +4,52 @@ import { createInterface } from 'readline';
 import Debug from '../../../../utils/Debug.js';
 
 const debug = Debug('parsePatents');
-export default async function firstPatentsImport(
-  filneName,
-  collection,
-  connection,
-) {
+export default async function firstPatentsImport(filneName, connection) {
   try {
+    const temporaryCollection = await connection.getCollection('patents_tpm');
     const readStream = createReadStream(filneName);
     const lines = createInterface({ input: readStream });
-    let entry = {};
-    let currentID = 1;
+    let entry = [];
+    let currentProductID = -1;
     let counter = 0;
     let start = Date.now();
+    let timeStartTenThousand = Date.now();
     for await (const line of lines) {
       let fields = line.split('\t');
       if (!fields.length === 2) continue;
       const [productID, patentID] = fields;
-      // if array size is greater than 500, skip till next productID
-      if (!entry[productID]) {
-        if (currentID !== Number(productID)) {
-          collection.updateOne(
-            { _id: Number(currentID) },
-            { $set: { data: entry[currentID] } },
-            { upsert: true },
-          );
-          delete entry[currentID];
-          currentID = Number(productID);
-          counter++;
+      if (currentProductID === -1) {
+        currentProductID = Number(productID);
+      }
+      if (currentProductID !== Number(productID)) {
+        await temporaryCollection.insertOne({
+          _id: Number(currentProductID),
+          data: { patents: entry.slice(0, 1000), nbPatents: entry.length },
+        });
+        entry.length = 0;
+        currentProductID = Number(productID);
+        counter++;
+        if (counter % 10000 === 0) {
+          const timeEndTenThousand = Date.now();
+          debug(`Time: ${timeEndTenThousand - timeStartTenThousand}`);
+          timeStartTenThousand = Date.now();
         }
-        entry[productID] = [];
       }
-      if (entry[productID].length > 500) {
-        continue;
-      }
-      entry[productID].push(patentID);
+      entry.push(patentID);
       if (Date.now() - start > 10000) {
         start = Date.now();
         debug(`Processed: ${counter} compounds`);
       }
     }
+    if (entry.length) {
+      await temporaryCollection.insertOne({
+        _id: Number(currentProductID),
+        data: { patents: entry.slice(0, 1000), nbPatents: entry.length },
+      });
+    }
+    await temporaryCollection.rename('patents', {
+      dropTarget: true,
+    });
   } catch (e) {
     if (connection) {
       debug(e.message, { collection: 'patents', connection, stack: e.stack });
