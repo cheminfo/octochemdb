5cc2c5384350f615dd4bbc42be4f4511794d9148
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-08-02 09:23:39 +0200

    fix: removed useless debug

--- pubchem/src/plugins/activesOrNaturals/aggregates/aggregateActivesOrNaturals.js
+++ pubchem/src/plugins/activesOrNaturals/aggregates/aggregateActivesOrNaturals.js
@@ -61,7 +61,7 @@ export async function aggregate(connection) {
     if (
       lastDocumentImported === null ||
       sources !== progress.sources ||
-      progress.state !== 'aggregated'
+      progress.state !== 'aggregatd'
     ) {
       // if lastDocumentImported is null or sources are different from the progress, start aggregation process
       const temporaryCollection = await connection.getCollection(
@@ -124,7 +124,6 @@ export async function aggregate(connection) {
               connection,
             );
             nbPubmeds += Number(counterPmids);
-            debug(counterPmids);
             meshTermsForCid.forEach((term) => {
               uniqueMeshTerms[term] = true;
             });
