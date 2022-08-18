bab8e9f340148b64358e23882897ca484e58b90c
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-08-18 10:13:56 +0200

    test: benchmark functions old and new for noStereoID #65

---
+++ pubchem/test/benchMarkNoStereoID.mjs
@@ -0,0 +1,28 @@
+import Benchmark from 'benchmark';
+import OCL from 'openchemlib';
+
+import getNoStereoIDCode from '../src/sync/utils/getNoStreoIDCode.js';
+
+// benchmark to get no stereo ID code with getNoStereoIDCode or with OCL stripStereoInformation method
+// 1. getNoStereoIDCode
+// 2. OCL stripStereoInformation
+// 3. OCL stripStereoInformation and getIDCodeAndCoordinates
+
+const suite = new Benchmark.Suite();
+suite
+  .add('old', () => {
+    const oclMolecule2 = OCL.Molecule.fromSmiles('C1=CC=C(C=C1)C2=CC=CC=C2');
+    oclMolecule2.stripStereoInformation();
+    const noStereoID2 = oclMolecule2.getIDCode();
+  })
+  .add('new', () => {
+    const oclMolecule = OCL.Molecule.fromSmiles('C1=CC=C(C=C1)C2=CC=CC=C2');
+    const noStereoID = getNoStereoIDCode(oclMolecule);
+  })
+  .on('cycle', (event) => {
+    console.log(String(event.target));
+  })
+  .on('complete', function onComplete() {
+    console.log(`Fastest is ${this.filter('fastest').map('name')}`);
+  })
+  .run();
