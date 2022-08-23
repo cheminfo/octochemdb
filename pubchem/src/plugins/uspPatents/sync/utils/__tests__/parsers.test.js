46998558e1976b26a8e4bda66df2d5ea8f42d327
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-08-23 16:41:19 +0200

    wip: test case parsers

---
+++ pubchem/src/plugins/uspPatents/sync/utils/__tests__/parsers.test.js
@@ -0,0 +1,66 @@
+import { open } from 'fs/promises';
+import { join } from 'path';
+
+import { parseStream } from 'arraybuffer-xml-parser';
+
+import { parsers } from '../parsers/parsers.js';
+
+describe('parsers', () => {
+  it('2001', async () => {
+    const xmlPath = join(__dirname, 'data/2001.xml');
+    const fileStream = await open(xmlPath, 'r');
+    const readableStream = fileStream.readableWebStream();
+    let count = 0;
+    let results;
+    for await (const entry of parseStream(
+      readableStream,
+      'patent-application-publication',
+    )) {
+      results = parsers(entry, '2001');
+      count++;
+      if (count > 0) {
+        break;
+      }
+    }
+    await fileStream.close();
+    expect(results).toMatchSnapshot();
+  });
+  it('2002', async () => {
+    const xmlPath = join(__dirname, 'data/2002.xml');
+    const fileStream = await open(xmlPath, 'r');
+    const readableStream = fileStream.readableWebStream();
+    let count = 0;
+    let results;
+    for await (const entry of parseStream(
+      readableStream,
+      'patent-application-publication',
+    )) {
+      results = parsers(entry, '2002');
+      count++;
+      if (count > 0) {
+        break;
+      }
+    }
+    await fileStream.close();
+    expect(results).toMatchSnapshot();
+  });
+  it('2005', async () => {
+    const xmlPath = join(__dirname, 'data/2005.xml');
+    const fileStream = await open(xmlPath, 'r');
+    const readableStream = fileStream.readableWebStream();
+    let count = 0;
+    let results;
+    for await (const entry of parseStream(
+      readableStream,
+      'us-patent-application',
+    )) {
+      results = parsers(entry, '2005');
+      count++;
+      if (count > 0) {
+        break;
+      }
+    }
+    await fileStream.close();
+    expect(results).toMatchSnapshot();
+  });
+});
