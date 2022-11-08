baf73bf8efa2d9899184660f5e642e1554d61417
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-11-08 14:05:38 +0100

    fix: try catch at wrong level

--- pubchem/src/plugins/compounds/sync/utils/getCompoundsData.js
+++ pubchem/src/plugins/compounds/sync/utils/getCompoundsData.js
@@ -12,45 +12,45 @@ const debug = Debug('getCompoundsData');
  * @returns  compounds properties
  */
 export async function getCompoundsData(molecule) {
-  let oclMolecule;
-  if (molecule.molfile) {
-    oclMolecule = OCL.Molecule.fromMolfile(molecule.molfile);
-  } else {
-    oclMolecule = OCL.Molecule.fromIDCode(molecule.idCode);
-  }
-  let idCode = oclMolecule.getIDCode();
-  const oclID = oclMolecule.getIDCodeAndCoordinates();
-  let urlIDCode = encodeURIComponent(idCode);
-  let dataCompound = await fetch(
-    `https://powernuc.cheminfo.org/v1/fromIDCode?idCode=${urlIDCode}`,
-  );
-  let data = await dataCompound.json();
-
-  let result = {
-    data: {
-      ocl: {
-        idCode: data.result.idCode,
-        coordinates: oclID.coordinates,
-        index: data.result.ssIndex,
-        noStereoID: data.result.noStereoID,
-        noStereoTautomerID: data.result.noStereoTautomerID,
-
-        acceptorCount: data.result.acceptorCount,
-        donorCount: data.result.donorCount,
-        logP: data.result.logP,
-        logS: data.result.logS,
-        polarSurfaceArea: data.result.polarSurfaceArea,
-        rotatableBondCount: data.result.rotatableBondCount,
-        stereoCenterCount: data.result.stereoCenterCount,
-      },
-      mf: data.result.mf,
-      em: data.result.em,
-      charge: data.result.charge,
-      mw: data.result.mw,
-    },
-  };
-
   try {
+    let oclMolecule;
+    if (molecule.molfile) {
+      oclMolecule = OCL.Molecule.fromMolfile(molecule.molfile);
+    } else {
+      oclMolecule = OCL.Molecule.fromIDCode(molecule.idCode);
+    }
+    let idCode = oclMolecule.getIDCode();
+    const oclID = oclMolecule.getIDCodeAndCoordinates();
+    let urlIDCode = encodeURIComponent(idCode);
+    let dataCompound = await fetch(
+      `https://powernuc.cheminfo.org/v1/fromIDCode?idCode=${urlIDCode}`,
+    );
+    let data = await dataCompound.json();
+
+    let result = {
+      data: {
+        ocl: {
+          idCode: data.result.idCode,
+          coordinates: oclID.coordinates,
+          index: data.result.ssIndex,
+          noStereoID: data.result.noStereoID,
+          noStereoTautomerID: data.result.noStereoTautomerID,
+
+          acceptorCount: data.result.acceptorCount,
+          donorCount: data.result.donorCount,
+          logP: data.result.logP,
+          logS: data.result.logS,
+          polarSurfaceArea: data.result.polarSurfaceArea,
+          rotatableBondCount: data.result.rotatableBondCount,
+          stereoCenterCount: data.result.stereoCenterCount,
+        },
+        mf: data.result.mf,
+        em: data.result.em,
+        charge: data.result.charge,
+        mw: data.result.mw,
+      },
+    };
+
     // calculate molecular formula properties (ex. exact mass, unsaturations, etc.)
     const mfInfo = new MF(result.data.mf).getInfo();
     result.data.unsaturation = mfInfo.unsaturation;
