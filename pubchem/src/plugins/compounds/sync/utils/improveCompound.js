317ba30b72f95af1f798ff6a2a47ec927acd5608
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-11-08 14:45:10 +0100

    fix: handle undefined response from query

--- pubchem/src/plugins/compounds/sync/utils/improveCompound.js
+++ pubchem/src/plugins/compounds/sync/utils/improveCompound.js
@@ -9,19 +9,21 @@ import { getCompoundsData } from './getCompoundsData.js';
  */
 async function improveCompound(molecule) {
   let dataCompound = await getCompoundsData(molecule);
-  let result = {
-    data: dataCompound.data,
-  };
-  if (molecule.molfile) {
-    result._id = +molecule.PUBCHEM_COMPOUND_CID;
-    result._seq = 0;
-    result.data.inchi = molecule.PUBCHEM_IUPAC_INCHI;
-    result.data.inchiKey = molecule.PUBCHEM_IUPAC_INCHIKEY;
-    result.data.iupac = molecule.PUBCHEM_IUPAC_NAME;
-    result.data.molfile = molecule.molfile;
-  }
+  if (dataCompound?.data) {
+    let result = {
+      data: dataCompound.data,
+    };
+    if (molecule.molfile) {
+      result._id = +molecule.PUBCHEM_COMPOUND_CID;
+      result._seq = 0;
+      result.data.inchi = molecule.PUBCHEM_IUPAC_INCHI;
+      result.data.inchiKey = molecule.PUBCHEM_IUPAC_INCHIKEY;
+      result.data.iupac = molecule.PUBCHEM_IUPAC_NAME;
+      result.data.molfile = molecule.molfile;
+    }
 
-  return result;
+    return result;
+  }
 }
 
 workerpool.worker({
