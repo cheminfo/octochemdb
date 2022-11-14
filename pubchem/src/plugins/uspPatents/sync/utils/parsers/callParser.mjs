6de3c625dd5237b856ebe2530a5c62db2963c5a7
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-11-14 16:53:42 +0100

    wip: parser for usp patents of 2006

--- pubchem/src/plugins/uspPatents/sync/utils/parsers/callParser.mjs
+++ pubchem/src/plugins/uspPatents/sync/utils/parsers/callParser.mjs
@@ -2,19 +2,19 @@ import { open } from 'fs/promises';
 
 import { parseStream } from 'arraybuffer-xml-parser';
 
-import { parseUsp2005 } from './parseUsp2005.js';
+import { parseUsp2006 } from './parseUsp2006.js';
 
 async function doAll() {
-  const xmlPath = '../__tests__/data/2005.xml';
+  const xmlPath = '../__tests__/data/2006.xml';
   const fileStream = await open(xmlPath, 'r');
   const readableStream = fileStream.readableWebStream();
   let count = 0;
   let results;
   for await (const entry of parseStream(
     readableStream,
-    'us-patent-application',
+    'patent-application-publication',
   )) {
-    results = parseUsp2005(entry);
+    results = parseUsp2006(entry);
     count++;
     if (count > 0) {
       break;
