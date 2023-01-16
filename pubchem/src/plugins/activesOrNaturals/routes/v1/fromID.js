6ea41ceef3926172f08baa5d799c5e28e56a5ebf
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-01-16 10:05:13 +0100

    chore: add cids to query

--- pubchem/src/plugins/activesOrNaturals/routes/v1/fromID.js
+++ pubchem/src/plugins/activesOrNaturals/routes/v1/fromID.js
@@ -29,7 +29,7 @@ export default entriesFromEM;
 async function searchHandler(request) {
   let {
     id = '',
-    fields = 'data.em,data.mf,data.charge,data.unsaturation,data.bioActive,data.naturalProduct,data.kwMeshTerms,data.kwBioassays,data.kwTaxonomies,data.kwActiveAgainst,data.activities,data.taxonomies,data.pubmeds,data.nbActivities,data.nbTaxonomies,data.nbPubmeds,data.nbPatents,data.patents,data.nbMassSpectra,data.massSpectraRefs',
+    fields = 'data.cids,data.em,data.mf,data.charge,data.unsaturation,data.bioActive,data.naturalProduct,data.kwMeshTerms,data.kwBioassays,data.kwTaxonomies,data.kwActiveAgainst,data.activities,data.taxonomies,data.pubmeds,data.nbActivities,data.nbTaxonomies,data.nbPubmeds,data.nbPatents,data.patents,data.nbMassSpectra,data.massSpectraRefs',
   } = request.query;
 
   let connection;
