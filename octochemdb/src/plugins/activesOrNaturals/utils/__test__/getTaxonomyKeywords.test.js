7befca720ff984d0d62d69a3307a6528bb8b82b2
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-01-31 09:38:06 +0100

    chore: test case

---
+++ octochemdb/src/plugins/activesOrNaturals/utils/__test__/getTaxonomyKeywords.test.js
@@ -0,0 +1,42 @@
+import getTaxonomyKeywords from '../getTaxonomyKeywords';
+
+test('getTaxonomyKeywords', () => {
+  const taxon = [
+    {
+      superKingdom: 'Bacteria',
+      kingdom: '',
+      phylum: 'Pseudomonadota',
+      class: 'Gammaproteobacteria',
+      order: 'enterobacteria',
+      family: 'enterobacteria',
+      genus: 'enterobacteria',
+      species: 'Escherichia coli',
+    },
+
+    {
+      superKingdom: 'eukaryota',
+      kingdom: 'Animalia',
+      phylum: 'chordata',
+      class: 'mammalia',
+      order: 'primates',
+      family: 'hominidae',
+      genus: 'Homo',
+      species: 'homo sapiens',
+    },
+  ];
+  const keywords = getTaxonomyKeywords(taxon);
+  expect(keywords).toStrictEqual([
+    'pseudomonadota',
+    'gammaproteobacteria',
+    'enterobacteria',
+    'escherichia',
+    'coli',
+    'animalia',
+    'chordata',
+    'mammalia',
+    'primates',
+    'hominidae',
+    'homo',
+    'sapiens',
+  ]);
+});
