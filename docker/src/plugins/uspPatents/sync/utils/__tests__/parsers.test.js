b5c05f21949c0c6ade673542d81d238694576668
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-02-20 10:14:11 +0100

    fix: parser 2002 already covers 2001

--- docker/src/plugins/uspPatents/sync/utils/__tests__/parsers.test.js
+++ docker/src/plugins/uspPatents/sync/utils/__tests__/parsers.test.js
@@ -6,25 +6,6 @@ import { parseStream } from 'arraybuffer-xml-parser';
 import { parsers } from '../parsers/parsers.js';
 
 describe('parsers', () => {
-  it('2001', async () => {
-    const xmlPath = join(__dirname, 'data/2001.xml');
-    const fileStream = await open(xmlPath, 'r');
-    const readableStream = fileStream.readableWebStream();
-    let count = 0;
-    let results;
-    for await (const entry of parseStream(
-      readableStream,
-      'patent-application-publication',
-    )) {
-      results = parsers(entry, '2001');
-      count++;
-      if (count > 0) {
-        break;
-      }
-    }
-    await fileStream.close();
-    expect(results).toMatchSnapshot();
-  });
   it('2002', async () => {
     const xmlPath = join(__dirname, 'data/2002.xml');
     const fileStream = await open(xmlPath, 'r');
