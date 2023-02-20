2c5d88a2fce55f7dc8b16af8f825477154a45e3e
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-02-20 10:09:24 +0100

    fix: removed useless tests

--- docker/src/plugins/activesOrNaturals/utils/__test__/testPubMedsCounter.test.js
+++
@@ -1,9 +0,0 @@
-import { PubChemConnection } from '../../../../utils/PubChemConnection.js';
-
-test('getPubMedsCounter', async () => {
-  const connection = new PubChemConnection();
-
-  const collection = await connection.getCollection('pubmeds');
-  const nbPubmeds = await collection.countDocuments({ 'data.cids': 5904 });
-  expect(nbPubmeds).toBe(10006);
-}, 5000);
