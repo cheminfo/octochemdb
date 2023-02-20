207c2d8c47f026f8c84dc2de3272f700ede0ad32
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-02-20 15:03:08 +0100

    fix: remove _seq from snapshots

--- docker/src/plugins/patents/sync/utils/__tests__/syncPatents.test.js
+++ docker/src/plugins/patents/sync/utils/__tests__/syncPatents.test.js
@@ -8,6 +8,8 @@ test('syncPatents', async () => {
   const collection = await connection.getCollection('patents');
   const collectionEntry = await collection.find({ _id: 5426 }).limit(1);
   const result = await collectionEntry.next();
+  // remove seq number
+  delete result._seq;
   expect(result).toMatchSnapshot();
   if (connection) {
     await connection.close();
--- docker/src/plugins/substances/sync/utils/__tests__/syncSubstances.test.js
+++ docker/src/plugins/substances/sync/utils/__tests__/syncSubstances.test.js
@@ -2,28 +2,32 @@ import { PubChemConnection } from '../../../../../utils/PubChemConnection.js';
 import { sync } from '../../syncSubstances';
 
 jest.setTimeout(300000);
-test('syncSubstances First Importation', async () => {
-  const connection = new PubChemConnection();
-  await sync(connection);
-  const collection = await connection.getCollection('substances');
-  const collectionEntry = await collection.find({ _id: 56427212 }).limit(1);
-  const result = await collectionEntry.next();
-  expect(result).toMatchSnapshot();
-  if (connection) {
-    await connection.close();
-  }
-});
-test('syncSubstances Incremental Importation', async () => {
-  const connection = new PubChemConnection();
-  const collection = await connection.getCollection('substances');
-  const collectionEntryIncremental = await collection
-    .find({ _id: 56435292 })
-    .limit(1);
-  const resultIncremental = await collectionEntryIncremental.next();
-  // remove seq number because the order of importation can change since it is done in parallel
-  delete resultIncremental.seq;
-  expect(resultIncremental).toMatchSnapshot();
-  if (connection) {
-    await connection.close();
-  }
+describe('syncSubstances', () => {
+  it('syncSubstances First Importation', async () => {
+    const connection = new PubChemConnection();
+    await sync(connection);
+    const collection = await connection.getCollection('substances');
+    const collectionEntry = await collection.find({ _id: 56427212 }).limit(1);
+    const result = await collectionEntry.next();
+    // remove seq number because the order of importation can change since it is done in parallel
+    delete result._seq;
+    expect(result).toMatchSnapshot();
+    if (connection) {
+      await connection.close();
+    }
+  });
+  it('syncSubstances Incremental Importation', async () => {
+    const connection = new PubChemConnection();
+    const collection = await connection.getCollection('substances');
+    const collectionEntryIncremental = await collection
+      .find({ _id: 56435292 })
+      .limit(1);
+    const resultIncremental = await collectionEntryIncremental.next();
+    // remove seq number because the order of importation can change since it is done in parallel
+    delete resultIncremental._seq;
+    expect(resultIncremental).toMatchSnapshot();
+    if (connection) {
+      await connection.close();
+    }
+  });
 });
