344cb6efcc3099a211757eb6f754865e6cc0dbc8
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-03-06 12:07:02 +0100

    chore: test case for getFileIfNew

---
+++ docker/src/sync/http/utils/__tests__/getFileIfNew.test.js
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+import { test, expect } from 'vitest';
+
+import getFileIfNew from '../getFileIfNew';
+
+test('getFileIfNew', async () => {
+  const destination = '../docker/src/sync/http/utils/__tests__/data/';
+  const fileName = 'testResult';
+  const extension = 'txt';
+  const options = {
+    filename: fileName,
+    extension,
+  };
+
+  // transform file path to url import.meta.url
+  const file = {
+    url: process.env.GETFILEIFNEW_SOURCE_TEST,
+  };
+
+  const result = await getFileIfNew(file, destination, options);
+  // search regex testResult
+  const regex = new RegExp(fileName, 'g');
+  const found = result.match(regex);
+  expect(found).toHaveLength(1);
+}, 10000);
--- docker/src/sync/http/utils/getFileIfNew.js
+++ docker/src/sync/http/utils/getFileIfNew.js
@@ -7,8 +7,9 @@ import {
 } from 'fs';
 import { join } from 'path';
 
-import { fileListFromPath } from 'filelist-utils';
+import { fileCollectionFromPath } from 'filelist-utils';
 import pkg from 'fs-extra';
+import fetch from 'node-fetch'; //ATTENTION: node-fetch is not the same as fetch
 
 import debugLibrary from '../../../utils/Debug.js';
 
@@ -28,11 +29,13 @@ async function getFileIfNew(file, targetFolder, options = {}) {
     throw new Error('options filename and extension are mandatory');
   }
   let target;
+
   try {
     const controller = new AbortController();
     setTimeout(() => controller.abort(), 1800 * 1000);
     mkdirpSync(targetFolder);
     const response = await fetch(file.url, { signal: controller.signal });
+
     if (response.status !== 200) {
       throw new Error(`Could not fetch file: ${file.url}`);
     }
@@ -44,7 +47,7 @@ async function getFileIfNew(file, targetFolder, options = {}) {
     let newFileSize = Number(
       headers.filter((row) => row[0] === 'content-length')[0][1],
     );
-    let fileList = fileListFromPath(targetFolder).filter(
+    let fileList = (await fileCollectionFromPath(targetFolder)).files.filter(
       (file) =>
         (file.name.includes('.zip') ||
           file.name.includes('.txt') ||
@@ -52,7 +55,7 @@ async function getFileIfNew(file, targetFolder, options = {}) {
           file.name.includes('.gz') ||
           file.name.includes('.tsv.gz')) &&
         file.name.includes(filename) &&
-        !file.webkitRelativePath.includes('old'),
+        !file.relativePath.includes('old'),
     );
     let lastFilesSize;
     let lastFileTargetLocal;
@@ -64,18 +67,20 @@ async function getFileIfNew(file, targetFolder, options = {}) {
     } else {
       lastFilesSize = 0;
     }
-
     if (lastFilesSize !== newFileSize) {
       let modificationDate = new Date(lastMofidied[1])
         .toISOString()
         .substring(0, 10);
       debug(`Last modification date: ${modificationDate}`);
-      if (!existsSync(join(targetFolder, 'old', modificationDate))) {
+      if (
+        !existsSync(join(targetFolder, 'old', modificationDate)) &&
+        process.env.NODE_ENV !== 'test'
+      ) {
         mkdirpSync(join(targetFolder, 'old', modificationDate));
       }
       fileList.forEach((file) => {
         renameSync(
-          file.webkitRelativePath,
+          file.relativePath,
           join(targetFolder, 'old', `${modificationDate}`, file.name),
         );
       });
