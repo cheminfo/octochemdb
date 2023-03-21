06ecf2d4feef94d3ee023f09799b07f48fb336a0
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-03-21 12:41:25 +0100

    fix: if connection error, retry after 10s

--- docker/src/plugins/uspPatents/sync/utils/http/getFileListUsp.js
+++ docker/src/plugins/uspPatents/sync/utils/http/getFileListUsp.js
@@ -1,48 +1,72 @@
 import delay from 'delay';
 
+import debugLibrary from '../../../../../utils/Debug.js';
+
 async function getFilesListUsp(url, year) {
-  let response = await fetch(`${url}${year}`);
-  let count = 0;
-  while (!response.ok && count < 3) {
-    delay(1000);
-    response = await fetch(`${url}${year}`);
-    count++;
-  }
-  const options = {};
-  const { fileFilter = () => true } = options;
-  const text = await response.text();
-  let files = text
-    .split(/\r?\n/)
-    .filter((line) => line.match(/\t<tr>/))
-    .map((line) => {
-      let match = line.match(/.*href="(?<href>.*)">(?<name>.*)<\/a>/);
-      if (!match) return undefined;
-      // groups object key are unprototype
-      let groups = match.groups;
-      if (match.groups.name) {
-        groups.url = `${url}${year}/${match.groups.name}`;
-        groups.epoch = new Date(`${year}`).getTime();
-      }
-      return groups;
-    })
-    .filter((file) => fileFilter(file));
-  files = JSON.parse(JSON.stringify(files));
-  // determine the file size of each file
-  for (let file of files) {
-    let responseFile = await fetch(file.url);
+  const debug = debugLibrary('getFileListUsp');
+  try {
+    let response;
+    let text;
     let counter = 0;
-    while (!responseFile.ok && counter < 3) {
-      delay(1000);
-      responseFile = await fetch(file.url);
-      counter++;
+    while (true) {
+      try {
+        response = await fetch(`${url}${year}`);
+        text = await response.text();
+        break;
+      } catch (e) {
+        if (counter++ > 10) {
+          throw e;
+        }
+        await delay(10000);
+      }
     }
-    const headers = Array.from(responseFile.headers);
-    const size = Number(
-      headers.filter((row) => row[0] === 'content-length')[0][1],
-    );
-    file.size = size;
+    const options = {};
+    const { fileFilter = () => true } = options;
+
+    //console.log('text', text);
+    let files = text
+      .split(/\r?\n/)
+      .filter((line) => line.match(/\t<tr>/))
+      .map((line) => {
+        let match = line.match(/.*href="(?<href>.*)">(?<name>.*)<\/a>/);
+        // console.log('match', match);
+        if (!match) return undefined;
+        // groups object key are unprototype
+        let groups = match.groups;
+        if (match.groups.name) {
+          groups.url = `${url}${year}/${match.groups.name}`;
+          groups.epoch = new Date(`${year}`).getTime();
+        }
+        return groups;
+      })
+      .filter((file) => fileFilter(file));
+    files = JSON.parse(JSON.stringify(files));
+
+    // determine the file size of each file
+    for (let file of files) {
+      let responseFile;
+      let count = 0;
+      while (true) {
+        try {
+          responseFile = await fetch(file.url);
+          break;
+        } catch (e) {
+          if (count++ > 10) {
+            throw e;
+          }
+          await delay(10000);
+        }
+      }
+      const headers = Array.from(responseFile.headers);
+      const size = Number(
+        headers.filter((row) => row[0] === 'content-length')[0][1],
+      );
+      file.size = size;
+    }
+    return files;
+  } catch (e) {
+    debug(e);
   }
-  return files;
 }
 
 export default getFilesListUsp;
