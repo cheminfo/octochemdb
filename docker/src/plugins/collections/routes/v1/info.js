98e89905a471d505234978114b843822f5e4af65
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-04-04 13:22:07 +0200

    chore: improved docs for routes of collectionsInfo

--- docker/src/plugins/collections/routes/v1/info.js
+++ docker/src/plugins/collections/routes/v1/info.js
@@ -7,6 +7,8 @@ const stats = {
   method: 'GET',
   schema: {
     summary: 'Retrieve an overview about all the collections',
+    description:
+      'Retrieve an overview (stored in admin) and statistics about all the collections',
   },
   handler: searchHandler,
 };
