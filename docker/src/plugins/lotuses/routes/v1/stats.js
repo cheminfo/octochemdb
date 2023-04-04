8d6e08069b6e6c6749256b3940662b395711f798
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-04-04 13:23:10 +0200

    chore: improved docs for routes of lotuses

--- docker/src/plugins/lotuses/routes/v1/stats.js
+++ docker/src/plugins/lotuses/routes/v1/stats.js
@@ -8,6 +8,8 @@ const stats = {
   method: 'GET',
   schema: {
     summary: 'Retrieve global statistics from the lotuses collection',
+    description:
+      'This route retrieves the global statics for the collection lotuses. This can be integrated in a monitoring system to keep under control the number of entries in the collection.',
   },
   handler: searchHandler,
 };
