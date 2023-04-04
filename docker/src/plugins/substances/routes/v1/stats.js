f016d8bdc8285b8dc285206b3473e14c98d4732d
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-04-04 13:25:13 +0200

    chore: improved docs for routes of substances

--- docker/src/plugins/substances/routes/v1/stats.js
+++ docker/src/plugins/substances/routes/v1/stats.js
@@ -7,7 +7,9 @@ const debug = debugLibrary('stats');
 const stats = {
   method: 'GET',
   schema: {
-    summary: 'Retrieve global statistics from the substance collection',
+    summary: 'Retrieve global statistics from the substances collection',
+    description:
+      'This route retrieves the global statics for the collection substances. This can be integrated in a monitoring system to keep under control the number of entries in the collection.',
   },
   handler: searchHandler,
 };
