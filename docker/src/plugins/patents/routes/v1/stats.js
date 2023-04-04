4bedf4f419a3016fe09437bb335c5d15c5736725
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-04-04 13:24:53 +0200

    chore: improved docs for routes of patents

--- docker/src/plugins/patents/routes/v1/stats.js
+++ docker/src/plugins/patents/routes/v1/stats.js
@@ -7,7 +7,9 @@ const debug = debugLibrary('stats');
 const stats = {
   method: 'GET',
   schema: {
-    summary: 'Retrieve global statistics from the npAtlases collection',
+    summary: 'Retrieve global statistics from the patents collection',
+    description:
+      'This route retrieves the global statics for the collection patents. This can be integrated in a monitoring system to keep under control the number of entries in the collection.',
   },
   handler: searchHandler,
 };
