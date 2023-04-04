904d6ea2fa012d9e02a61365b7c863153c9316fc
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-04-04 14:23:00 +0200

    fix: increase timeout test

--- docker/src/plugins/activesOrNaturals/utils/__test__/aggregateActivesOrNaturals.test.js
+++ docker/src/plugins/activesOrNaturals/utils/__test__/aggregateActivesOrNaturals.test.js
@@ -4,38 +4,34 @@ import { test, expect } from 'vitest';
 import { OctoChemConnection } from '../../../../utils/OctoChemConnection.js';
 import { aggregate } from '../../aggregates/aggregateActivesOrNaturals';
 
-test(
-  'Aggregation ActivesOrNaturals',
-  async () => {
-    const connection = new OctoChemConnection();
-    let colllectionList = await connection.getCollectionNames();
-    while (
-      !colllectionList.includes('lotuses') ||
-      !colllectionList.includes('npasses') ||
-      !colllectionList.includes('npAtlases') ||
-      !colllectionList.includes('cmaups') ||
-      !colllectionList.includes('coconuts') ||
-      !colllectionList.includes('bioassays') ||
-      !colllectionList.includes('gnps') ||
-      !colllectionList.includes('pubmeds') ||
-      !colllectionList.includes('patents')
-    ) {
-      await delay(1000);
-      colllectionList = await connection.getCollectionNames();
-    }
-    await aggregate(connection);
-    const collection = await connection.getCollection('activesOrNaturals');
-    const collectionEntry = await collection
-      .find({
-        _id: 'ekTpA@@@LAEMGLn\\dTTRbRfLbteRrRTfbqbtRthdRjZFFfNnAQjjjjjjjfjjjjjijjh@@',
-      })
-      .limit(1);
-    const result = await collectionEntry.next();
-    if (result?._seq) {
-      delete result._seq;
-    }
-    expect(result).toMatchSnapshot();
-    await connection.close();
-  },
-  { timeout: 60000 },
-);
+test('Aggregation ActivesOrNaturals', async () => {
+  const connection = new OctoChemConnection();
+  let colllectionList = await connection.getCollectionNames();
+  while (
+    !colllectionList.includes('lotuses') ||
+    !colllectionList.includes('npasses') ||
+    !colllectionList.includes('npAtlases') ||
+    !colllectionList.includes('cmaups') ||
+    !colllectionList.includes('coconuts') ||
+    !colllectionList.includes('bioassays') ||
+    !colllectionList.includes('gnps') ||
+    !colllectionList.includes('pubmeds') ||
+    !colllectionList.includes('patents')
+  ) {
+    await delay(1000);
+    colllectionList = await connection.getCollectionNames();
+  }
+  await aggregate(connection);
+  const collection = await connection.getCollection('activesOrNaturals');
+  const collectionEntry = await collection
+    .find({
+      _id: 'ekTpA@@@LAEMGLn\\dTTRbRfLbteRrRTfbqbtRthdRjZFFfNnAQjjjjjjjfjjjjjijjh@@',
+    })
+    .limit(1);
+  const result = await collectionEntry.next();
+  if (result?._seq) {
+    delete result._seq;
+  }
+  expect(result).toMatchSnapshot();
+  await connection.close();
+}, 100000);
