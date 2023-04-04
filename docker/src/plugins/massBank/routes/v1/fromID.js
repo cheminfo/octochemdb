89d07036f932515668cc26b5f26d138642313399
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-04-04 13:23:31 +0200

    feat: routes for massBank collection

---
+++ docker/src/plugins/massBank/routes/v1/fromID.js
@@ -0,0 +1,59 @@
+import { OctoChemConnection, getFields } from '../../../../server/utils.js';
+import debugLibrary from '../../../../utils/Debug.js';
+
+const debug = debugLibrary('fromID');
+
+const fromID = {
+  method: 'GET',
+  schema: {
+    summary: 'Retrieve mass spectrum from MassBank ID',
+    description: '',
+    querystring: {
+      id: {
+        type: 'string',
+        description: 'MassBank ID',
+        example: 'MSBNK-AAFC-AC000854',
+        default: 'MSBNK-AAFC-AC000854',
+      },
+      fields: {
+        type: 'string',
+        description: 'Fields to retrieve',
+        default: 'data.ocl.noStereoTautomerID, data.spectrum',
+      },
+    },
+  },
+  handler: searchHandler,
+};
+
+export default fromID;
+
+async function searchHandler(request) {
+  let { id = '', fields = 'data.ocl.noStereoTautomerID' } = request.query;
+
+  let connection;
+  try {
+    connection = new OctoChemConnection();
+    // get the collection
+    const collection = await connection.getCollection('massBank');
+
+    debug(id);
+    const results = await collection
+      .aggregate([
+        { $match: { _id: id } },
+        { $limit: 1 },
+        {
+          $project: getFields(fields),
+        },
+      ])
+      .next();
+    return { data: results };
+  } catch (e) {
+    if (connection) {
+      debug(e.message, { collection: 'massBank', connection, stack: e.stack });
+    }
+    return { errors: [{ title: e.message, detail: e.stack }] };
+  } finally {
+    debug('Closing connection');
+    if (connection) await connection.close();
+  }
+}
---
+++ docker/src/plugins/massBank/routes/v1/fromMasses.js
@@ -0,0 +1,99 @@
+import { getFields, OctoChemConnection } from '../../../../server/utils.js';
+import debugLibrary from '../../../../utils/Debug.js';
+
+const debug = debugLibrary('fromMasses');
+// export the handler
+const fromMasses = {
+  method: 'GET',
+  schema: {
+    summary: 'Retrieve mass spectra from range of masses',
+    description:
+      'Allows to search for mass spectra based on a range of m/z values.',
+    querystring: {
+      masses: {
+        type: 'string',
+        description: 'List Monoisotopic mass',
+        example: '125.02,185.09',
+        default: '125.02,185.09',
+      },
+      precision: {
+        type: 'number',
+        description: 'Precision (in ppm) of the monoisotopic mass',
+        default: 10,
+      },
+      limit: {
+        type: 'number',
+        description: 'Maximum number of results to return',
+        default: 10,
+      },
+      fields: {
+        type: 'string',
+        description: 'Fields to retrieve',
+        default: 'data.spectrum,data.ocl',
+      },
+    },
+  },
+  handler: searchHandler,
+};
+export default fromMasses;
+
+/**
+ * @description Search for compounds from a monoisotopic mass, target taxonomies, source taxonomies and bioassays
+ * @param {object} request
+ * @returns {Promise<object>} Entries who match the query parameters inside the activeOrNaturals collection
+ */
+async function searchHandler(request) {
+  let {
+    masses = '125.02,185.09',
+    precision = 10,
+    limit = 10,
+    fields = 'data.spectrum,data.ocl',
+  } = request.query;
+
+  // define the error allowed for the search
+  let connection;
+  try {
+    connection = new OctoChemConnection();
+    const collection = await connection.getCollection('massBank');
+    // get the fields to be retrieved
+    let formattedFields = getFields(fields);
+    formattedFields._id = 0;
+
+    let massesArray = masses.split(/[, \t\n\r:;]+/);
+    let matchParameters = [];
+    for (let massString of massesArray) {
+      const mass = Number(massString);
+      const error = (mass / 1e6) * precision;
+      matchParameters.push({
+        'data.spectrum.data.x': {
+          $elemMatch: {
+            $gte: mass - error,
+            $lte: mass + error,
+          },
+        },
+      });
+    }
+    // search for the entries
+    const results = await collection
+      .aggregate([
+        { $match: { $and: matchParameters } },
+        { $limit: limit },
+        {
+          $project: formattedFields,
+        },
+      ])
+      .toArray();
+    return { data: results };
+  } catch (e) {
+    if (connection) {
+      debug(e.message, {
+        collection: 'massBank',
+        connection,
+        stack: e.stack,
+      });
+    }
+    return { errors: [{ title: e.message, detail: e.stack }] };
+  } finally {
+    if (connection) await connection.close();
+  }
+}
