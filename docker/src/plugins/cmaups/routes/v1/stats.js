fb6fe6e4e39b3babb749f0c2f2c749ed8d531641
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-04-04 13:21:32 +0200

    chore: improved docs for routes of cmaups

--- docker/src/plugins/cmaups/routes/v1/stats.js
+++ docker/src/plugins/cmaups/routes/v1/stats.js
@@ -7,6 +7,8 @@ const stats = {
   method: 'GET',
   schema: {
     summary: 'Retrieve global statistics from the cmaups collection',
+    description:
+      'This route retrieves the global statics for the collection cmaups. This can be integrated in a monitoring system to keep under control the number of entries in the collection.',
   },
   handler: searchHandler,
 };
