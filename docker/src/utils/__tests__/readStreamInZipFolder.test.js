417285f775a07d03294f189d098bf48c3a5229f3
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-04-14 10:57:39 +0200

    fixL text case

--- docker/src/utils/__tests__/readStreamInZipFolder.test.js
+++ docker/src/utils/__tests__/readStreamInZipFolder.test.js
@@ -7,15 +7,14 @@ import readStreamInZipFolder from '../readStreamInZipFolder';
 test('readStreamInZipFolder', async () => {
   let path = `../docker/src/utils/__tests__/data/`;
 
-  const fileToRead = (await fileCollectionFromPath(`${path}`)).files.filter(
-    (file) => {
-      return (
-        file.relativePath.includes('zip') && file.name === 'testCoconut.bson'
-      );
-    },
-  )[0];
+  const fileToRead = (
+    await fileCollectionFromPath(`${path}`, { unzip: { zipExtensions: [] } })
+  ).files.sort((a, b) => b.lastModified - a.lastModified)[0];
   fileToRead.relativePath = path.replace('data/', fileToRead.relativePath);
-  const readStream = await readStreamInZipFolder(fileToRead);
+  const readStream = await readStreamInZipFolder(
+    fileToRead.relativePath,
+    'testCoconut.bson',
+  );
   let result = [];
   for await (const entry of bsonIterator(readStream)) {
     result.push(entry);
