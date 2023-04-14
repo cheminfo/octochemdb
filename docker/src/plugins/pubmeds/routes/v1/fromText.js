2715e8f0d9113c83be2f40320424ef7eff547227
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-04-14 14:55:17 +0200

    chore: route search fromText for pubmed

---
+++ docker/src/plugins/pubmeds/routes/v1/fromText.js
@@ -0,0 +1,85 @@
+// query for molecules from monoisotopic mass
+import { OctoChemConnection, getFields } from '../../../../server/utils.js';
+import debugLibrary from '../../../../utils/Debug.js';
+
+const debug = debugLibrary('fromText');
+
+const fromText = {
+  method: 'GET',
+  schema: {
+    summary:
+      'Retrieve articles which title, MeSH terms or abstract contains the given text',
+    description: 'Allows to search for articles Title and Abstract.',
+    querystring: {
+      wordsToSearch: {
+        type: 'string',
+        description:
+          'Text to be searched in articles Title, Abstract or MeSH terms',
+        example: 'antibiotic',
+        default: '',
+      },
+      fields: {
+        type: 'string',
+        description: 'Fields to retrieve',
+        default: 'data',
+      },
+      minScore: {
+        type: 'number',
+        description: 'Text search score minimum',
+        example: 2,
+        default: 1,
+      },
+    },
+  },
+  handler: searchHandler,
+};
+
+export default fromText;
+
+async function searchHandler(request) {
+  let {
+    wordsToSearch = '',
+    fields = 'data.article.title, _id, data.article.abstract,data.meshHeadings',
+    minScore = 0,
+  } = request.query;
+  let formattedFields = getFields(fields);
+  formattedFields.score = { $meta: 'textScore' };
+  let connection;
+  try {
+    connection = new OctoChemConnection();
+    const collection = await connection.getCollection('pubmeds');
+    let wordsToBeMatched = '';
+
+    let words = wordsToSearch
+      .toLowerCase()
+      .split(/ *[,;\t\n\r\s]+ */)
+      .filter((entry) => entry);
+    for (let word of words) {
+      // eslint-disable-next-line no-useless-escape
+      word = `\"${word}\"`;
+      wordsToBeMatched = wordsToBeMatched.concat(word, ' ');
+    }
+    const result = await collection
+      .aggregate([
+        { $match: { $text: { $search: wordsToBeMatched } } },
+        { $sort: { score: { $meta: 'textScore' } } },
+        { $project: formattedFields },
+        { $match: { score: { $gt: minScore } } },
+      ])
+      .toArray();
+
+    return { data: result };
+  } catch (e) {
+    if (connection) {
+      debug(e.message, {
+        collection: 'pubmeds',
+        connection,
+        stack: e.stack,
+      });
+    }
+    return { errors: [{ title: e.message, detail: e.stack }] };
+  } finally {
+    debug('Closing connection');
+    if (connection) await connection.close();
+  }
+}
