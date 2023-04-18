316c5f7e1dffe853f10f5f82d10a6e5f9e1950f2
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-04-18 11:49:22 +0200

    fix: continue next iteration if error

--- docker/src/plugins/massBank/sync/utils/parseMassBank.js
+++ docker/src/plugins/massBank/sync/utils/parseMassBank.js
@@ -11,45 +11,59 @@ export async function* parseMassBank(blob, connection) {
   try {
     const parsedData = parseMSP(blob);
     for (let data of parsedData) {
-      let result = {};
-      if (data.meta) {
-        result._id = data.meta['DB#'];
+      try {
+        let result = {};
+        if (data.meta) {
+          result._id = data.meta['DB#'];
 
-        let oclMolecule = OCL.Molecule.fromSmiles(data.meta.SMILES);
-        let ocl = await getNoStereosFromCache(oclMolecule, connection);
-        result.data = {
-          ocl,
-        };
-        let dataPeaks = { x: data.variables.x.data, y: data.variables.y.data };
-        let spectrumToBeFilter = new Spectrum(dataPeaks);
-        let minMaxX = spectrumToBeFilter.minMaxX();
-        let slots = (minMaxX.max - minMaxX.min) / 0.1 - 1;
-        xNormed(spectrumToBeFilter.data.y, {
-          algorithm: 'max',
-          output: spectrumToBeFilter.data.y,
-        });
-        let bestPeaks = spectrumToBeFilter.getBestPeaks({
-          numberSlots: slots,
-          numberCloseSlots: slots,
-          limit: 100,
-          threshold: 0.01,
-        });
-        let bestPeaksXY = xyObjectToXY(bestPeaks);
-        result.data.spectrum = { data: {} };
-        result.data.spectrum.data.x = bestPeaksXY.x;
-        result.data.spectrum.data.y = bestPeaksXY.y;
-        result.data.spectrum.numberOfPeaks = bestPeaks.length;
-        result.data.spectrum.instrument = data.meta.Instrument;
-        result.data.spectrum.instrumentType = data.meta.Instrument_type;
-        result.data.spectrum.ionMode = data.meta.Ion_mode;
-        result.data.spectrum.adduct = data.meta.Spectrum_type;
-        result.data.spectrum.collisionEnergy = data.meta.Collision_energy;
-        result.data.spectrum.msLevel = data.meta.Spectrum_type.replace(
-          /MS/,
-          '',
-        );
+          let oclMolecule = OCL.Molecule.fromSmiles(data.meta.SMILES);
+          let ocl = await getNoStereosFromCache(oclMolecule, connection);
+          result.data = {
+            ocl,
+          };
+          let dataPeaks = {
+            x: data.variables.x.data,
+            y: data.variables.y.data,
+          };
+          let spectrumToBeFilter = new Spectrum(dataPeaks);
+          let minMaxX = spectrumToBeFilter.minMaxX();
+          let slots = (minMaxX.max - minMaxX.min) / 0.1 - 1;
+          xNormed(spectrumToBeFilter.data.y, {
+            algorithm: 'max',
+            output: spectrumToBeFilter.data.y,
+          });
+          let bestPeaks = spectrumToBeFilter.getBestPeaks({
+            numberSlots: slots,
+            numberCloseSlots: slots,
+            limit: 100,
+            threshold: 0.01,
+          });
+          let bestPeaksXY = xyObjectToXY(bestPeaks);
+          result.data.spectrum = { data: {} };
+          result.data.spectrum.data.x = bestPeaksXY.x;
+          result.data.spectrum.data.y = bestPeaksXY.y;
+          result.data.spectrum.numberOfPeaks = bestPeaks.length;
+          result.data.spectrum.instrument = data.meta.Instrument;
+          result.data.spectrum.instrumentType = data.meta.Instrument_type;
+          result.data.spectrum.ionMode = data.meta.Ion_mode;
+          result.data.spectrum.adduct = data.meta.Spectrum_type;
+          result.data.spectrum.collisionEnergy = data.meta.Collision_energy;
+          result.data.spectrum.msLevel = data.meta.Spectrum_type.replace(
+            /MS/,
+            '',
+          );
+        }
+        yield result;
+      } catch (e) {
+        if (connection) {
+          debug(e.message, {
+            collection: 'massbank',
+            connection,
+            stack: e.stack,
+          });
+        }
+        continue;
       }
-      yield result;
     }
   } catch (e) {
     debug(e.message, {
