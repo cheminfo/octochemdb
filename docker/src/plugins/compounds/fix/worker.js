b0e932daaf73e9a6fbc025a95f7f1d2548cbe700
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-05-04 10:06:00 +0200

    fix: do not erased if already corrected

--- docker/src/plugins/compounds/fix/worker.js
+++ docker/src/plugins/compounds/fix/worker.js
@@ -1,7 +1,11 @@
 import { parentPort } from 'worker_threads';
 
+import OCL from 'openchemlib';
+
+import debugLibrary from '../../../utils/Debug.js';
 import { OctoChemConnection } from '../../../utils/OctoChemConnection.js';
 
+const debug = debugLibrary('fix');
 const connection = new OctoChemConnection();
 parentPort?.on('message', async (entryData) => {
   try {
@@ -17,9 +21,21 @@ parentPort?.on('message', async (entryData) => {
       let entry = await collectionEntry.next();
       if (entry?.data) {
         let atoms = JSON.parse(entry?.data?.atoms);
-        let index = Array.from(
-          new Int32Array(new Uint8Array(entry?.data.index).buffer),
-        );
+        let index;
+        if (
+          entry?.data?.ocl?.index !== null &&
+          entry?.data?.ocl?.index.length > 0
+        ) {
+          index = Array.from(
+            new Int32Array(new Uint8Array(entry?.data.index).buffer),
+          );
+          if (index === null) {
+            index = entry?.data.index;
+          }
+        } else {
+          let molecule = OCL.Molecule.fromMolfile(entry?.data?.molfile);
+          index = molecule.getIndex();
+        }
         // updateOne
         await collection.updateOne(
           { _id: entry._id },
@@ -30,12 +46,12 @@ parentPort?.on('message', async (entryData) => {
       }
     }
     if (Date.now() - start > 1000) {
-      console.log(`Worker ${workerID} fixed ${count} compounds`);
+      debug(`Worker ${workerID} fixed ${count} compounds`);
       start = Date.now();
     }
     count++;
     parentPort.postMessage(`${workerID} fixed ${count} compounds`);
   } catch (e) {
-    console.log(e);
+    debug(e);
   }
 });
