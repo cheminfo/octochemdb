d2f463d52d3373a238e182649c8b2b91efbb2fad
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-05-10 16:40:07 +0200

    fix: check if target taxonomies are not undefined

--- docker/src/plugins/activesOrNaturals/utils/getActivitiesInfo.js
+++ docker/src/plugins/activesOrNaturals/utils/getActivitiesInfo.js
@@ -28,7 +28,7 @@ export default async function getActivitiesInfo(data, connection) {
       if (entry.collection === 'npasses' || entry.collection === 'cmaups') {
         if (entry.data.activities) {
           for (let activity of entry.data.activities) {
-            if (activity?.targetTaxonomies !== null) {
+            if (activity?.targetTaxonomies !== undefined) {
               activity.targetTaxonomies = [activity.targetTaxonomies];
             }
             activityInfo.push(activity);
