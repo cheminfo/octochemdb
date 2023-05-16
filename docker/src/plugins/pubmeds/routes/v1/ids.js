674cfdc65a52c5869b517772676e0f2a6299f063
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-05-16 14:28:36 +0200

    feat: route to search ids in pubmed

---
+++ docker/src/plugins/pubmeds/routes/v1/ids.js
@@ -0,0 +1,86 @@
+// query for molecules from monoisotopic mass
+import { OctoChemConnection, getFields } from '../../../../server/utils.js';
+import debugLibrary from '../../../../utils/Debug.js';
+
+const debug = debugLibrary('searchIDs');
+
+const searchIDs = {
+  method: ['GET', 'POST'],
+  schema: {
+    summary:
+      'Retrieve articles which title, MeSH terms or abstract contains the given text',
+    description: 'Allows to search for articles Title and Abstract.',
+    querystring: {
+      ids: {
+        type: 'string',
+        description: 'PubMed IDs comma separated',
+        example: '19342308,17200418',
+        default: '',
+      },
+      fields: {
+        type: 'string',
+        description: 'Fields to retrieve',
+        default: 'data',
+      },
+    },
+  },
+  handler: searchHandler,
+};
+
+export default searchIDs;
+
+async function searchHandler(request) {
+  let data;
+  if (request.method === 'GET') {
+    data = request.query;
+  } else {
+    data = request.body;
+  }
+  let {
+    ids = '',
+
+    fields = 'data',
+  } = data;
+
+  let formattedFields = getFields(fields);
+  let connection;
+  try {
+    connection = new OctoChemConnection();
+    const collection = await connection.getCollection('pubmeds');
+    let matchParameters = {};
+    let aggregateParameters;
+
+    if (ids !== '') {
+      matchParameters._id = {
+        $in: ids
+          .split(/[ ,;\t\r\n]+/)
+          .filter((entry) => entry)
+          .map(Number),
+      };
+      aggregateParameters = [
+        {
+          $match: matchParameters,
+        },
+        { $project: formattedFields },
+      ];
+    } else {
+      return { error: [{ title: 'No IDs provided' }] };
+    }
+
+    const result = await collection.aggregate(aggregateParameters).toArray();
+
+    return { data: result };
+  } catch (e) {
+    if (connection) {
+      await debug.error(e.message, {
+        collection: 'pubmeds',
+        connection,
+        stack: e.stack,
+      });
+    }
+    return { errors: [{ title: e.message, detail: e.stack }] };
+  } finally {
+    debug.trace('Closing connection');
+    if (connection) await connection.close();
+  }
+}
