b1a90a1b0e2e2e8d6cde246dce7cbcf7ad00a6f7
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-05-16 14:31:35 +0200

    feat: route ids for patents

---
+++ docker/src/plugins/patents/routes/v1/ids.js
@@ -0,0 +1,76 @@
+import { OctoChemConnection, getFields } from '../../../../server/utils.js';
+import debugLibrary from '../../../../utils/Debug.js';
+
+const debug = debugLibrary('ids');
+
+const searchIDs = {
+  method: ['GET', 'POST'],
+  schema: {
+    summary:
+      'Retrieve articles which title or abstract contains the given text',
+    description: 'Allows to search for articles Title and Abstract.',
+    querystring: {
+      patentsIDs: {
+        type: 'string',
+        description: 'patents IDs',
+        example: 'EP-2078065-A2, EP-1293521-A2',
+        default: '',
+      },
+      fields: {
+        type: 'string',
+        description: 'Fields to retrieve',
+        default: 'data',
+      },
+    },
+  },
+  handler: searchHandler,
+};
+
+export default searchIDs;
+
+async function searchHandler(request) {
+  let data;
+  if (request.method === 'GET') {
+    data = request.query;
+  } else {
+    data = request.body;
+  }
+  let { patentsIDs = '', fields = 'data' } = data;
+  let formattedFields = getFields(fields);
+  let connection;
+  try {
+    connection = new OctoChemConnection();
+    const collection = await connection.getCollection('patents');
+
+    let matchParameters = {};
+    let aggregateParameters;
+
+    if (patentsIDs !== '') {
+      matchParameters._id = { $in: patentsIDs.split(/[ ,;\t\r\n]+/) };
+      aggregateParameters = [
+        {
+          $match: matchParameters,
+        },
+        { $project: formattedFields },
+      ];
+    } else {
+      return { error: [{ title: 'No patents IDs provided' }] };
+    }
+
+    const result = await collection.aggregate(aggregateParameters).toArray();
+
+    return { data: result };
+  } catch (e) {
+    if (connection) {
+      await debug.error(e.message, {
+        collection: 'patents',
+        connection,
+        stack: e.stack,
+      });
+    }
+    return { errors: [{ title: e.message, detail: e.stack }] };
+  } finally {
+    debug.trace('Closing connection');
+    if (connection) await connection.close();
+  }
+}
