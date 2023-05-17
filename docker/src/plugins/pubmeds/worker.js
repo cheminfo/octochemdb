b207dd9cb08bd999373e691c7115b621bb49952a
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-05-17 09:00:27 +0200

    chore: convert data.compounds to dbrefs

---
+++ docker/src/plugins/pubmeds/worker.js
@@ -0,0 +1,52 @@
+import { parentPort } from 'worker_threads';
+
+import debug from 'debug';
+
+import { OctoChemConnection } from '../../utils/OctoChemConnection';
+
+const connection = new OctoChemConnection();
+parentPort?.on('message', async (entryData) => {
+  try {
+    let { links, workerID } = entryData;
+
+    let count = 0;
+    const collection = await connection.getCollection('pubmeds');
+
+    let start = Date.now();
+    for (let seq = links[0]; seq <= links[1]; seq++) {
+      const collectionEntry = await collection.find({ _seq: Number(seq) });
+      let entry = await collectionEntry.next();
+      if (Date.now() - start > 60000) {
+        // @ts-ignore
+        parentPort.postMessage({
+          workerID,
+          currentCount: count,
+          status: 'running',
+        });
+        start = Date.now();
+      }
+      count++;
+
+      if (entry?.data?.compounds) {
+        let compounds = entry.data.compounds;
+        let dbRefs = [];
+        for (let compound of compounds) {
+          dbRefs.push({ $ref: 'compounds', $id: compound });
+        }
+        delete entry.data.compounds;
+        entry.data.compounds = dbRefs;
+        await collection.updateOne(
+          { _id: entry._id },
+          { $set: { data: entry.data } },
+        );
+      } else {
+        continue;
+      }
+    }
+
+    // @ts-ignore
+    parentPort.postMessage({ workerID, currentCount: count, status: 'done' });
+  } catch (e) {
+    debug(e);
+  }
+});
---
+++ docker/src/plugins/substances/fix.mjs
@@ -0,0 +1,56 @@
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+
+import debug from 'debug';
+
+import { OctoChemConnection } from '../../utils/OctoChemConnection';
+
+export async function main() {
+  const connection = new OctoChemConnection();
+  const colletion = await connection.getCollection('substances');
+  const total = await colletion.countDocuments();
+  const url = new URL('worker.js', import.meta.url);
+  const numWorkers = cpus().length / 4;
+  const chunkSize = Math.floor(total / numWorkers);
+  let workers = [];
+  for (let i = 0; i < numWorkers; i++) {
+    const start = i * chunkSize;
+    const end = i === numWorkers - 1 ? total : (i + 1) * chunkSize;
+    const chunk = [start, end];
+    let worker = new Worker(url);
+    worker.postMessage({ links: chunk, workerID: i });
+    workers.push(worker);
+  }
+  const counts = new Uint32Array(numWorkers);
+  let lastLogDate = Date.now();
+  await Promise.all(
+    workers.map(
+      (worker) =>
+        new Promise((resolve, reject) => {
+          worker.on('message', (message) => {
+            counts[message.workerID] = message.currentCount;
+            if (
+              Date.now() - lastLogDate > Number(process.env.DEBUG_THROTTLING) &&
+              message.status === 'running'
+            ) {
+              let current = counts.reduce(
+                (previous, current) => previous + current,
+                0,
+              );
+              lastLogDate = Date.now();
+              debug(`Processing: ${current} / ${total} `);
+            }
+            if (message.status === 'done') {
+              resolve(message);
+            }
+          });
+          worker.on('error', reject);
+          worker.on('exit', (code) => {
+            if (code !== 0) {
+              reject(new Error(`Worker stopped with exit code ${code}`));
+            }
+          });
+        }),
+    ),
+  );
+}
