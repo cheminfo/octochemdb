c4580235efcaefec4266917914eceda954f2fce0
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-05-23 12:12:10 +0200

    fix: test case

--- docker/src/plugins/activesOrNaturals/utils/__test__/getMeshTerms.test.js
+++ docker/src/plugins/activesOrNaturals/utils/__test__/getMeshTerms.test.js
@@ -9,11 +9,25 @@ test(
   async () => {
     const connection = new OctoChemConnection();
     let colllectionList = await connection.getCollectionNames();
-    while (!colllectionList.includes('pubmeds')) {
+    while (
+      !colllectionList.includes('pubmeds') &&
+      !colllectionList.includes('activesOrNaturals')
+    ) {
       await delay(1000);
       colllectionList = await connection.getCollectionNames();
     }
     const collection = await connection.getCollection('pubmeds');
+    await collection.updateMany(
+      {},
+      {
+        $set: {
+          'data.compounds': [
+            { $ref: 'compounds', $id: 19342306 },
+            { $ref: 'compounds', $id: 19342308 },
+          ],
+        },
+      },
+    );
     let cids = [
       { $ref: 'compounds', $id: 19342306 },
       { $ref: 'compounds', $id: 19342308 },
