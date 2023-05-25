42bc51095fc8587d7eb64dd77655f3a22d0d9786
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-05-25 11:41:29 +0200

    fix: keep first idCodes

--- docker/src/plugins/activesOrNaturals/utils/getCIDs.js
+++ docker/src/plugins/activesOrNaturals/utils/getCIDs.js
@@ -15,33 +15,35 @@ export default async function getCIDs(connection, noStereoTautomerID, data) {
     .toArray();
   let cids = [];
   let cidsDBRef = [];
-  let dbRefsMolecules = [];
+
   let idCodes = [];
+  let molecules = {};
   for (let idCode of result) {
     idCodes.push(idCode.idCode);
   }
-  if (result.length > 0) {
-    for (let cid of result) {
-      // create a DBRef for each cid
-      cidsDBRef.push({ $ref: 'compounds', $id: cid.cid });
-      cids.push(cid.cid);
-      dbRefsMolecules.push({
-        $ref: 'compounds',
-        $id: cid.cid,
-      });
-      for (let oneDataEntry of data) {
-        if (
-          !idCodes.includes(oneDataEntry.data.ocl.idCode) &&
-          oneDataEntry.collection !== 'bioassays'
-        ) {
-          dbRefsMolecules.push({
-            $ref: oneDataEntry.collection,
-            $id: oneDataEntry._id,
-          });
-        }
-      }
-      dbRefsMolecules = [...new Set(dbRefsMolecules)];
+
+  for (let cid of result) {
+    // create a DBRef for each cid
+    cidsDBRef.push({ $ref: 'compounds', $id: cid.cid });
+    cids.push(cid.cid);
+    molecules[cid.idCode] = {
+      $ref: 'compounds',
+      $id: cid.cid,
+    };
+  }
+  for (let oneDataEntry of data) {
+    if (
+      !idCodes.includes(oneDataEntry.data.ocl.idCode) &&
+      oneDataEntry.collection !== 'bioassays' &&
+      // check if the molecule is not already in the list
+      molecules[oneDataEntry.data.ocl.idCode] === undefined
+    ) {
+      molecules[oneDataEntry.data.ocl.idCode] = {
+        $ref: oneDataEntry.collection,
+        $id: oneDataEntry._id,
+      };
     }
   }
+  let dbRefsMolecules = Object.values(molecules);
   return { cids, cidsDBRef, dbRefsMolecules };
 }
