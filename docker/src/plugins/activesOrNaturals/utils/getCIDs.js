ae34df12d41cbe89b21e8714f511f7dfdf28b125
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-06-05 15:58:11 +0200

    feat!: Links to natural products sources closes: https://github.com/cheminfo/octochemdb/issues/114

--- docker/src/plugins/activesOrNaturals/utils/getCIDs.js
+++ docker/src/plugins/activesOrNaturals/utils/getCIDs.js
@@ -9,39 +9,53 @@ export default async function getCIDs(connection, noStereoTautomerID, data) {
           // get the id named as cid
           cid: '$_id',
           idCode: '$data.ocl.idCode',
+          coordinates: '$data.ocl.coordinates',
         },
       },
     ])
     .toArray();
+
   let cids = [];
   let cidsDBRef = [];
 
-  let idCodes = [];
   let molecules = {};
-  for (let idCode of result) {
-    idCodes.push(idCode.idCode);
-  }
 
   for (let cid of result) {
     // create a DBRef for each cid
     cidsDBRef.push({ $ref: 'compounds', $id: cid.cid });
     cids.push(cid.cid);
-    molecules[cid.idCode] = {
+    if (molecules[cid.idCode] === undefined) {
+      molecules[cid.idCode] = {
+        ocl: {
+          idCode: cid.idCode,
+          coordinates: cid.coordinates,
+        },
+        sources: [],
+      };
+    }
+    molecules[cid.idCode].sources.push({
       $ref: 'compounds',
       $id: cid.cid,
-    };
+    });
   }
   for (let oneDataEntry of data) {
     if (
-      !idCodes.includes(oneDataEntry.data.ocl.idCode) &&
       oneDataEntry.collection !== 'bioassays' &&
-      // check if the molecule is not already in the list
-      molecules[oneDataEntry.data.ocl.idCode] === undefined
+      !molecules[oneDataEntry.data.ocl.idCode === undefined]
     ) {
       molecules[oneDataEntry.data.ocl.idCode] = {
+        ocl: {
+          idCode: oneDataEntry.data.ocl.idCode,
+          coordinates: oneDataEntry.data.ocl.coordinates,
+        },
+        sources: [],
+      };
+    }
+    if (oneDataEntry.collection !== 'bioassays') {
+      molecules[oneDataEntry.data.ocl.idCode].sources.push({
         $ref: oneDataEntry.collection,
         $id: oneDataEntry._id,
-      };
+      });
     }
   }
   let dbRefsMolecules = Object.values(molecules);
