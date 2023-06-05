caa54e84d53c1673e82616b958861c492362a909
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-06-05 10:44:47 +0200

    chore: test case for titleCompounds

---
+++ docker/src/plugins/titleCompounds/sync/utils/__tests__/syncTitleCompounds.test.js
@@ -0,0 +1,14 @@
+import { test, expect } from 'vitest';
+
+import { OctoChemConnection } from '../../../../../utils/OctoChemConnection.js';
+import { sync } from '../../syncTitleCompounds.js';
+
+test('syncTitleCompounds', async () => {
+  const connection = new OctoChemConnection();
+  await sync(connection);
+  const collection = await connection.getCollection('titleCompounds');
+  const collectionEntry = await collection.find({ _id: 8366 }).limit(1);
+  const result = await collectionEntry.next();
+  expect(result?.data.title).toMatchInlineSnapshot('"Etryptamine acetate"');
+  await connection.close();
+}, 100000);
