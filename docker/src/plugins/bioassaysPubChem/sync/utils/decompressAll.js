235c2b2d1b30cbf6ec41fcc0a4797e0c37111b66
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-09-20 10:50:13 +0200

    fix: use streams to avoid memory limit

--- docker/src/plugins/bioassaysPubChem/sync/utils/decompressAll.js
+++ docker/src/plugins/bioassaysPubChem/sync/utils/decompressAll.js
@@ -1,4 +1,4 @@
-import { existsSync, mkdirSync, rmSync, writeFileSync } from 'fs';
+import { existsSync, mkdirSync, rmSync, createWriteStream } from 'fs';
 import { join } from 'path';
 
 import { fileCollectionFromPath } from 'filelist-utils';
@@ -13,21 +13,42 @@ import debugLibrary from '../../../../utils/Debug.js';
 export async function decompressAll(zipPath) {
   const debug = debugLibrary('decompressAll');
   let extractedFiles = [];
+  const writePromises = []; // Array to store the write stream promises
+
   try {
     const fileCollection = await fileCollectionFromPath(zipPath);
     // create a directory with the same name as the zip file
     if (!existsSync(zipPath.replace('.zip', ''))) {
       mkdirSync(zipPath.replace('.zip', ''));
     }
+
     for (const file of fileCollection.files) {
       const outputFilePath = join(zipPath.replace('.zip', ''), file.name);
       extractedFiles.push(outputFilePath);
-      const json = JSON.parse(await file.text());
-      writeFileSync(outputFilePath, JSON.stringify(json, null, 2));
+
+      // write stream using array buffer
+      const arrayBuffer = await file.arrayBuffer();
+      const buffer = Buffer.from(arrayBuffer);
+
+      // Create a promise for the write stream and store it in the array
+      const writePromise = new Promise((resolve, reject) => {
+        const stream = createWriteStream(outputFilePath);
+        stream.write(buffer);
+        stream.end();
+        stream.on('finish', resolve);
+        stream.on('error', reject);
+      });
+
+      writePromises.push(writePromise);
     }
+
+    // Wait for all write stream promises to resolve
+    await Promise.all(writePromises);
+
     if (process.env.NODE_ENV !== 'test' && existsSync(zipPath)) {
       rmSync(zipPath);
     }
+
     return extractedFiles;
   } catch (e) {
     debug.error(e);
