50a0d6f08f80061c00ea6ef12bd77f980363d541
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-10-16 13:26:02 +0200

    feat(npAtlases): cleaner routes

--- docker/src/plugins/npAtlases/routes/v1/stats.js
+++ docker/src/plugins/npAtlases/routes/v1/stats.js
@@ -1,8 +1,4 @@
-// query for molecules from monoisotopic mass
-import { OctoChemConnection } from '../../../../server/utils.js';
-import debugLibrary from '../../../../utils/Debug.js';
-
-const debug = debugLibrary('stats');
+import { statsHandler } from './searchHandlers/statsHandler';
 
 const stats = {
   method: 'GET',
@@ -11,35 +7,7 @@ const stats = {
     description:
       'This route retrieves the global statics for the collection npAtlases. This can be integrated in a monitoring system to keep under control the number of entries in the collection.',
   },
-  handler: searchHandler,
+  handler: statsHandler,
 };
 
 export default stats;
-/**
- * Returns statistics about the collection
- * @return {Promise} - Promise that resolves to an object with the statistics
- */
-
-async function searchHandler() {
-  let connection;
-  try {
-    connection = new OctoChemConnection();
-    const collection = await connection.getCollection('npAtlases');
-
-    const results = await collection.stats();
-
-    return { data: results };
-  } catch (e) {
-    if (connection) {
-      await debug.error(e.message, {
-        collection: 'npAtlases',
-        connection,
-        stack: e.stack,
-      });
-    }
-    return { errors: [{ title: e.message, detail: e.stack }] };
-  } finally {
-    debug.trace('Closing connection');
-    if (connection) await connection.close();
-  }
-}
