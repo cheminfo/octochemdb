31e5d4811f9a8309bb7230a089395d063c785e76
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-10-16 12:32:10 +0200

    feat(admin): cleaner routes

--- docker/src/plugins/admin/routes/v1/stats.js
+++ docker/src/plugins/admin/routes/v1/stats.js
@@ -1,45 +1,13 @@
-// query for molecules from monoisotopic mass
-import { OctoChemConnection } from '../../../../server/utils.js';
-import debugLibrary from '../../../../utils/Debug.js';
+import { statsHandler } from './searchHandlers/statsHandler.js';
 
-const debug = debugLibrary('stats');
-// export default searchHandler;
 const stats = {
   method: 'GET',
+  handler: statsHandler,
   schema: {
     summary: 'Retrieve global statistics from the admin collection',
     description:
       'This route retrieves the global statics for the collection admin. This can be integrated in a monitoring system to keep under control the number of entries in the collection.',
   },
-  handler: searchHandler,
 };
 
 export default stats;
-/**
- * @description get the global statistics from the admin collection
- * @return {Promise} Returns statistics about the collection
- */
-
-async function searchHandler() {
-  let connection;
-  try {
-    connection = new OctoChemConnection();
-    const collection = await connection.getCollection('admin');
-
-    const results = await collection.stats();
-
-    return { data: results };
-  } catch (e) {
-    if (connection) {
-      await debug.fatal(e.message, {
-        collection: 'admin',
-        connection,
-        stack: e.stack,
-      });
-    }
-    return { errors: [{ title: e.message, detail: e.stack }] };
-  } finally {
-    debug.trace('Closing connection');
-    if (connection) await connection.close();
-  }
-}
