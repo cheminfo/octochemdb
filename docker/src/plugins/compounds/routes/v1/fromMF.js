3661a91f26496a77947c1ac233888ef1bd61ce0a
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-10-16 12:51:14 +0200

    feat(compounds): cleaner routes

--- docker/src/plugins/compounds/routes/v1/fromMF.js
+++ docker/src/plugins/compounds/routes/v1/fromMF.js
@@ -1,8 +1,4 @@
-// query for molecules from monoisotopic mass
-import { getFields, OctoChemConnection } from '../../../../server/utils.js';
-import debugLibrary from '../../../../utils/Debug.js';
-
-const debug = debugLibrary('fromMF');
+import { fromMFHandler } from './searchHandlers/fromMFHandler';
 
 const fromMF = {
   method: 'GET',
@@ -30,47 +26,7 @@ const fromMF = {
       },
     },
   },
-  handler: searchHandler,
+  handler: fromMFHandler,
 };
 
 export default fromMF;
-
-async function searchHandler(request) {
-  let {
-    mf = '',
-    limit = 1e3,
-    fields = 'data.em,data.mf,data.unsaturation,data.charge,data.ocl.idCode',
-  } = request.query;
-
-  if (limit > 5e4) limit = 5e4;
-  if (limit < 1) limit = 1;
-
-  let connection;
-  try {
-    connection = new OctoChemConnection();
-    const collection = await connection.getCollection('compounds');
-
-    const results = await collection
-      .aggregate([
-        { $match: { 'data.mf': mf } },
-        { $limit: limit },
-        {
-          $project: getFields(fields),
-        },
-      ])
-      .toArray();
-    return { data: results };
-  } catch (e) {
-    if (connection) {
-      await debug.fatal(e.message, {
-        collection: 'compounds',
-        connection,
-        stack: e.stack,
-      });
-    }
-    return { errors: [{ title: e.message, detail: e.stack }] };
-  } finally {
-    debug.trace('Closing connection');
-    if (connection) await connection.close();
-  }
-}
--- docker/src/plugins/compounds/routes/v1/fromSmiles.js
+++ docker/src/plugins/compounds/routes/v1/fromSmiles.js
@@ -1,10 +1,4 @@
-import OCL from 'openchemlib';
-
-import { getFields, OctoChemConnection } from '../../../../server/utils.js';
-import debugLibrary from '../../../../utils/Debug.js';
-import { getNoStereosFromCache } from '../../../../utils/getNoStereosFromCache.js';
-
-const debug = debugLibrary('fromSmiles');
+import { fromSmilesHandler } from './searchHandlers/fromSmilesHandler';
 
 const fromSmiles = {
   method: 'GET',
@@ -36,65 +30,7 @@ const fromSmiles = {
       },
     },
   },
-  handler: searchHandler,
+  handler: fromSmilesHandler,
 };
 
 export default fromSmiles;
-
-async function searchHandler(request) {
-  let {
-    smiles = '',
-    limit = 1e3,
-    stereo = true,
-    fields = 'data.em,data.mf,data.total,data.atom,data.unsaturation',
-  } = request.query;
-
-  let connection;
-  try {
-    if (limit > 1e4) limit = 1e4;
-    if (limit < 1) limit = 1;
-
-    const molecule = OCL.Molecule.fromSmiles(smiles);
-    let mongoQuery = {};
-    if (stereo) {
-      mongoQuery = {
-        'data.ocl.idCode': molecule.getIDCode(),
-      };
-    } else {
-      let stereoCache = await getNoStereosFromCache(
-        molecule,
-        connection,
-        'compounds',
-      );
-      mongoQuery = {
-        'data.ocl.noStereoTautomerID': stereoCache?.noStereoTautomerID,
-      };
-    }
-
-    connection = new OctoChemConnection();
-    const collection = await connection.getCollection('compounds');
-
-    const results = await collection
-      .aggregate([
-        { $match: mongoQuery },
-        { $limit: Number(limit) },
-        {
-          $project: getFields(fields),
-        },
-      ])
-      .toArray();
-    return { data: results };
-  } catch (e) {
-    if (connection) {
-      await debug.fatal(e.message, {
-        collection: 'compounds',
-        connection,
-        stack: e.stack,
-      });
-    }
-    return { errors: [{ title: e.message, detail: e.stack }] };
-  } finally {
-    debug.trace('Closing connection');
-    if (connection) await connection.close();
-  }
-}
