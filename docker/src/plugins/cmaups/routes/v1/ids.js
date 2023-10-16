7f494699165b21b6a56e1722ca469f7442e000a6
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-10-16 12:38:24 +0200

    feat(cmaups): cleaner routes

--- docker/src/plugins/cmaups/routes/v1/ids.js
+++ docker/src/plugins/cmaups/routes/v1/ids.js
@@ -1,12 +1,8 @@
-// query for molecules from monoisotopic mass
-import { OctoChemConnection, getFields } from '../../../../server/utils.js';
-import debugLibrary from '../../../../utils/Debug.js';
-import { getRequestQuery } from '../../../../utils/getRequestQuery.js';
-
-const debug = debugLibrary('searchIDs');
+import { idsHandler } from './searchHandlers/idsHandler';
 
 const searchIDs = {
   method: ['GET', 'POST'],
+  handler: idsHandler,
   schema: {
     summary: 'Retrieve cmaups entries for given cmaups IDs',
     description: 'Allows to search for compounds data',
@@ -25,50 +21,6 @@ const searchIDs = {
       },
     },
   },
-  handler: searchHandler,
 };
 
 export default searchIDs;
-
-async function searchHandler(request) {
-  let data = getRequestQuery(request);
-  let { ids = '', fields = 'data' } = data;
-
-  let formattedFields = getFields(fields);
-  let connection;
-  try {
-    connection = new OctoChemConnection();
-    const collection = await connection.getCollection('cmaups');
-    let matchParameters = {};
-    let aggregateParameters;
-
-    if (ids !== '') {
-      matchParameters._id = {
-        $in: ids.split(/[ ,;\t\r\n]+/).filter((entry) => entry),
-      };
-    }
-
-    aggregateParameters = [
-      {
-        $match: matchParameters,
-      },
-      { $project: formattedFields },
-    ];
-
-    const result = await collection.aggregate(aggregateParameters).toArray();
-
-    return { data: result };
-  } catch (e) {
-    if (connection) {
-      await debug.fatal(e.message, {
-        collection: 'cmaups',
-        connection,
-        stack: e.stack,
-      });
-    }
-    return { errors: [{ title: e.message, detail: e.stack }] };
-  } finally {
-    debug.trace('Closing connection');
-    if (connection) await connection.close();
-  }
-}
