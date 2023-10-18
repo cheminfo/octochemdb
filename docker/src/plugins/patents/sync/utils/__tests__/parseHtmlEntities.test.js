1e3cbb1c434487e5b2519a04b60582d097fbb11b
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-10-18 12:46:30 +0200

    feat(patents): parse also HTML Named entities

---
+++ docker/src/plugins/patents/sync/utils/__tests__/parseHtmlEntities.test.js
@@ -0,0 +1,19 @@
+import { test, expect } from 'vitest';
+
+import { parseHtmlEntities } from '../parseHtmlEntities';
+
+test('parseHtmlEntities', async () => {
+  const text1 =
+    'CAMEO Chemicals is a chemical database designed for people who are involved in hazardous material incident response and planning. CAMEO Chemicals contains a library with thousands of datasheets containing response-related information and recommendations for hazardous materials that are commonly transported, used, or stored in the United States. CAMEO Chemicals was developed by the National Oceanic and Atmospheric Administration&apos;s Office of Response and Restoration in partnership with the Environmental Protection Agency&apos;s Office of Emergency Management.';
+  const text2 =
+    'https://www.ilo.org/dyn/icsc/showcard.display?p_version=2&amp;p_card_id=043';
+
+  const result1 = parseHtmlEntities(text1);
+  const result2 = parseHtmlEntities(text2);
+  expect(result2).toMatchInlineSnapshot(
+    '"https://www.ilo.org/dyn/icsc/showcard.display?p_version=2&p_card_id=043"',
+  );
+  expect(result1).toMatchInlineSnapshot(
+    '"CAMEO Chemicals is a chemical database designed for people who are involved in hazardous material incident response and planning. CAMEO Chemicals contains a library with thousands of datasheets containing response-related information and recommendations for hazardous materials that are commonly transported, used, or stored in the United States. CAMEO Chemicals was developed by the National Oceanic and Atmospheric Administration\'s Office of Response and Restoration in partnership with the Environmental Protection Agency\'s Office of Emergency Management."',
+  );
+});
--- docker/src/plugins/patents/sync/utils/parseHtmlEntities.js
+++ docker/src/plugins/patents/sync/utils/parseHtmlEntities.js
@@ -1,12 +1,56 @@
 export function parseHtmlEntities(str) {
-  return str.replace(
-    /\\u(?<temp2>[\d\w]{4})|&#(?<temp1>[0-9]{1,3});/gi,
-    (match, grp1, grp2) => {
-      if (grp1) {
-        return String.fromCharCode(parseInt(grp1, 16)); //Unicode value with \u it is interpreted as a hexadecimal value
-      } else if (grp2) {
-        return String.fromCharCode(parseInt(grp2, 10));
+  if (typeof str !== 'string') {
+    str = JSON.stringify(str);
+  }
+
+  // First, handle numeric entities
+  str = str.replace(
+    /\\u(?<unicode>[\d\w]{4})|&#(?<htmlEntity>[0-9]{1,3});/gi,
+    (match, _, __, unicode, htmlEntity) => {
+      if (unicode) {
+        return String.fromCharCode(parseInt(unicode, 16));
+      } else if (htmlEntity) {
+        return String.fromCharCode(parseInt(htmlEntity, 10));
       }
+      return match;
     },
   );
+
+  const namedEntities = {
+    '&nbsp;': ' ',
+    '&amp;': '&',
+    '&lt;': '<',
+    '&gt;': '>',
+    '&quot;': '"',
+    '&apos;': "'",
+    '&cent;': '¢',
+    '&pound;': '£',
+    '&yen;': '¥',
+    '&euro;': '€',
+    '&copy;': '©',
+    '&reg;': '®',
+    '&sect;': '§',
+    '&laquo;': '«',
+    '&raquo;': '»',
+    '&bull;': '•',
+    '&ndash;': '–',
+    '&mdash;': '—',
+    '&lsquo;': '‘',
+    '&rsquo;': '’',
+    '&ldquo;': '“',
+    '&rdquo;': '”',
+    '&trade;': '™',
+    '&times;': '×',
+    '&divide;': '÷',
+    '&oslash;': 'ø',
+    '&Oslash;': 'Ø',
+    '&plusmn;': '±',
+    '&micro;': 'µ',
+  };
+
+  for (const [entity, replacement] of Object.entries(namedEntities)) {
+    str = str.split(entity).join(replacement);
+  }
+
+  return str;
 }
